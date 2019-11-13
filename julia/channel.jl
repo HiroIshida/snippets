5f6b43ee9fae8b7782981170e1d64ed31d219a22
HiroIshida <spitfire.docomo@gmail.com>
2019-11-13 16:16:38 +0900

    add julia tips

---
+++ julia/channel.jl
@@ -0,0 +1,26 @@
+# same as generator
+function producer(c::Channel)
+  put!(c, 1)
+  put!(c, 3)
+  put!(c, 5)
+end
+
+for e in Channel(producer)
+  println(e)
+end
+
+# the following code using do-block works same as the code above
+c = Channel() do c
+  put!(c, 1)
+  put!(c, 3)
+  put!(c, 5)
+end
+
+for e in c
+  println(e)
+end
+
+
+
+
+
