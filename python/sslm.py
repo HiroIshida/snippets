c4f8a3f57355bf71746a5ce3e54ff85035d9af43
HiroIshida <spitfire.docomo@gmail.com>
2019-11-30 23:39:03 +0900

    fanctionize

--- python/sslm.py
+++ python/sslm.py
@@ -34,39 +34,47 @@ def gen_ymat(y):
     ymat = tmp * tmp.T
     return ymat
 
-nu1 = 0.1
-nu2 = 0.1
-nu = 0.2
+def solve_sslm(X, y, N, n_p):
+    nu1 = 0.1
+    nu2 = 0.1
+    nu = 0.2
+
+    Ymat = gen_ymat(y)
+
+    gram = linear_kernel(X.T)
+    gram_diag_matrix = np.diag(gram)
+
+    P = cvxopt.matrix(gram * Ymat)
+    q = cvxopt.matrix(
+            - np.array([-gram_diag_matrix]).T * 
+            np.array([y]).T)
+
+    # eq 15
+    A_15 = np.array([y],dtype = np.float64)
+    b_15 = np.eye(1)
+    A_16 = np.ones((1, N))
+    b_16 = np.eye(1)*(2 * nu + 1)
+    A = cvxopt.matrix(np.vstack((A_15, A_16)))
+    b = cvxopt.matrix(np.vstack((b_15, b_16)))
+
+    G0 = np.eye(N)
+    G1 = - np.eye(N)
+    G = cvxopt.matrix(np.vstack((G0, G1)))
+
+    h0p = np.ones(n_p)/(nu1 * n_p)
+    h0m = np.ones(N - n_p)/(nu2 * (N - n_p))
+    h1 = np.zeros(N)
+    h = cvxopt.matrix(np.block([h0p, h0m, h1]))
+    sol = cvxopt.solvers.qp(P, q, A=A, b=b, G=G, h=h)
+    return sol
 
 N = 100
 X, y, n_p = gen_dataset(N)
-Ymat = gen_ymat(y)
 
-gram = linear_kernel(X.T)
-gram_diag_matrix = np.diag(gram)
+sol = solve_sslm(X, y, N, n_p)
 
-P = cvxopt.matrix(gram * Ymat)
-q = cvxopt.matrix(
-        - np.array([-gram_diag_matrix]).T * 
-        np.array([y]).T)
 
-# eq 15
-A_15 = np.array([y],dtype = np.float64)
-b_15 = np.eye(1)
-A_16 = np.ones((1, N))
-b_16 = np.eye(1)*(2 * nu + 1)
-A = cvxopt.matrix(np.vstack((A_15, A_16)))
-b = cvxopt.matrix(np.vstack((b_15, b_16)))
 
-G0 = np.eye(N)
-G1 = - np.eye(N)
-G = cvxopt.matrix(np.vstack((G0, G1)))
 
-h0p = np.ones(n_p)/(nu1 * n_p)
-h0m = np.ones(N - n_p)/(nu2 * (N - n_p))
-h1 = np.zeros(N)
-h = cvxopt.matrix(np.block([h0p, h0m, h1]))
-
-sol = cvxopt.solvers.qp(P, q, A=A, b=b, G=G, h=h)
 xs = [sol["x"][i] for i in range(N)]
 
