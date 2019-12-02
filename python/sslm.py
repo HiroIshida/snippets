46befa5b17b648d8bd7994de36c78840e1d71516
HiroIshida <spitfire.docomo@gmail.com>
2019-12-03 02:17:25 +0900

    use class

--- python/sslm.py
+++ python/sslm.py
@@ -38,7 +38,7 @@ def show2d(func, bmin, bmax, N = 20, fax = None, levels = None):
     fig.colorbar(cf)
 
 
-def gen_dataset_(N):
+def gen_dataset(N):
     predicate = lambda x: x[0]**2 + x[1]**2 < 0.5 ** 2
     xp_lst = []
     xm_lst = []
@@ -60,14 +60,6 @@ def gen_dataset_(N):
     Y = np.array(y_lst)
     return X, Y, n_p
 
-def gen_dataset():
-    X = np.array([
-        [0.0, 0.0],
-        [0.5, 1.0]
-        ])
-    y = np.array([1, -1])
-    return X, y, 1
-
 def gen_diadig(vec):
     n = vec.size
     Y = np.array([vec])
@@ -75,103 +67,121 @@ def gen_diadig(vec):
     ymat = tmp * tmp.T
     return ymat
 
+class SSLM:
 
-N = 60
-#X, y, n_p = gen_dataset()
-X, y, n_p = gen_dataset_(N)
-kern = linear_kernel
+    def __init__(self, X, y, kern, nu = 0.1, nu1 = 0.01, nu2 = 0.02):
+        self.X = X
+        self.y = y
+        self.kern = kern
+        self.nu = nu
+        self.nu1 = nu1 
+        self.nu2 = nu2
 
-nu1 = 0.01
-nu2 = 0.01
-nu = 0.2
+        self.N = len(y)
+        self.m1 = (self.N + sum(y))/2
+        self.m2 = N - self.m1
 
-Ymat = gen_diadig(np.array(y))
+        self.R, self.rho, self.cc, self.a_lst, self.idxes_SVp, self.idxes_SVn\
+                = self._compute_important_parameters()
 
-gram = kern(X.T)
-gram_diag_matrix = np.diag(gram)
+    def predict(self, x):
+        val = (self.R**2 - self.cc - self.kern(x, x) + sum(2 * self.kern(X.T, x).flatten() * self.a_lst * self.y)).item()
+        return val
 
-gramymat = gram * Ymat
-gramymat_diag = np.array([-gram_diag_matrix]).T * np.array([y]).T
+    def _compute_important_parameters(self):
+        gram, gramymat, a_lst = self._solve_qp()
 
-P = cvxopt.matrix(gramymat)
-q = cvxopt.matrix(gramymat_diag)
+        eps = 0.00001
+        idxes_S1 = []
+        for i in range(self.m1):
+            a = a_lst[i]
+            if eps < a and a < 1.0/(self.nu1 * self.m1) - eps:
+                idxes_S1.append(i)
 
-# eq 15
-A_15 = np.array([y],dtype = np.float64)
-b_15 = np.eye(1)
-A_16 = np.ones((1, N))
-b_16 = np.eye(1)*(2 * nu + 1)
-A_ = np.vstack((A_15, A_16))
-B_ = np.vstack((b_15, b_16))
-A = cvxopt.matrix(A_)
-b = cvxopt.matrix(B_)
+        idxes_S2 = []
+        for i in range(self.m1, self.m1 + self.m2):
+            a = a_lst[i]
+            if eps < a and a < 1.0/(self.nu2 * self.m2) - eps:
+                idxes_S2.append(i)
 
-G0 = np.eye(N)
-G1 = - np.eye(N)
-G_ = np.vstack((G0, G1))
-G = cvxopt.matrix(G_)
+        cc = sum(sum(gen_diadig(a_lst) * gramymat))
+        f_inner = lambda idx: gram[idx, idx] - sum(2 * gram[idx, :] * a_lst * self.y) + cc
 
-h0p = np.ones(n_p)/(nu1 * n_p)
-h0m = np.ones(N - n_p)/(nu2 * (N - n_p))
-h1 = np.zeros(N)
-h_ = np.block([h0p, h0m, h1])
-h = cvxopt.matrix(h_)
-sol = cvxopt.solvers.qp(P, q, A=A, b=b, G=G, h=h)
-a_lst = np.array([sol["x"][i] for i in range(N)])
+        P1 = sum(map(f_inner, idxes_S1))
+        P2 = sum(map(f_inner, idxes_S2))
 
-m1 = n_p
-m2 = N - n_p
+        n1 = len(idxes_S1)
+        n2 = len(idxes_S2)
+        R = math.sqrt(P1/n1)
+        rho = math.sqrt(P2/n2 - P1/n1)
+        return R, rho, cc, a_lst, idxes_S1, idxes_S2
 
-eps = 0.00001
-idxes_S1 = []
-for i in range(m1):
-    a = a_lst[i]
-    if eps < a and a < 1.0/(nu1 * m1) - eps:
-        idxes_S1.append(i)
+    def _solve_qp(self):
+        Ymat = gen_diadig(np.array(self.y))
 
-idxes_S2 = []
-for i in range(m1, m1 + m2):
-    a = a_lst[i]
-    if eps < a and a < 1.0/(nu2 * m2) - eps:
-        idxes_S2.append(i)
+        gram = kern(self.X.T)
+        gram_diag_matrix = np.diag(gram)
 
-cc = sum(sum(gen_diadig(a_lst) * gramymat))
-f_inner = lambda idx: gram[idx, idx] - sum(2 * gram[idx, :] * a_lst * y) + cc
+        gramymat = gram * Ymat
+        gramymat_diag = np.array([-gram_diag_matrix]).T * np.array([self.y]).T
 
-P1 = sum(map(f_inner, idxes_S1))
-P2 = sum(map(f_inner, idxes_S2))
+        P = cvxopt.matrix(gramymat)
+        q = cvxopt.matrix(gramymat_diag)
 
-n1 = len(idxes_S1)
-n2 = len(idxes_S2)
-R = math.sqrt(P1/n1)
-rho = math.sqrt(P2/n2 - P1/n1)
+        # eq 15
+        A_15 = np.array([self.y],dtype = np.float64)
+        b_15 = np.eye(1)
+        A_16 = np.ones((1, self.N))
+        b_16 = np.eye(1)*(2 * self.nu + 1)
+        A_ = np.vstack((A_15, A_16))
+        B_ = np.vstack((b_15, b_16))
+        A = cvxopt.matrix(A_)
+        b = cvxopt.matrix(B_)
 
-Xt = X.T
-x_new = Xt[1, :]
-predict = lambda x: (R**2 - cc - kern(x, x) + sum(2 * kern(X.T, x).flatten() * a_lst * y)).item()
-predict([0, 0.8])
+        G0 = np.eye(self.N)
+        G1 = - np.eye(self.N)
+        G_ = np.vstack((G0, G1))
+        G = cvxopt.matrix(G_)
+
+        h0p = np.ones(self.m1)/(self.nu1 * self.m1)
+        h0m = np.ones(self.N - self.m1)/(self.nu2 * (self.N - self.m1))
+        h1 = np.zeros(self.N)
+        h_ = np.block([h0p, h0m, h1])
+        h = cvxopt.matrix(h_)
+        sol = cvxopt.solvers.qp(P, q, A=A, b=b, G=G, h=h)
+        a_lst = np.array([sol["x"][i] for i in range(self.N)])
+        return gram, gramymat, a_lst
+
+if __name__ == '__main__':
+    N = 100
+    X, y, n_p = gen_dataset(N)
+    kern = linear_kernel
+
+    sslm = SSLM(X, y, kern)
+    sslm.predict([0, 0.8])
 
 
-## plot
+    ## plot
 
-fig, ax = plt.subplots() 
+    fig, ax = plt.subplots() 
 
 
-bmin = np.array([-1, -1])
-bmax = np.array([1, 1])
-def f(x):
-    tmp = predict(x)
-    return tmp, tmp
-show2d(f, bmin, bmax, fax = (fig, ax))
+    bmin = np.array([-1, -1])
+    bmax = np.array([1, 1])
+    def f(x):
+        tmp = sslm.predict(x)
+        return tmp, tmp
+    show2d(f, bmin, bmax, fax = (fig, ax))
 
-idx_positive = np.where(y == 1)
-idx_negative = np.where(y == -1)
+    idx_positive = np.where(y == 1)
+    idx_negative = np.where(y == -1)
 
-plt.scatter(X[0, idx_positive], X[1, idx_positive], c = "blue")
-plt.scatter(X[0, idx_negative], X[1, idx_negative], c = "red")
+    plt.scatter(X[0, idx_positive], X[1, idx_positive], c = "blue")
+    plt.scatter(X[0, idx_negative], X[1, idx_negative], c = "red")
 
-plt.scatter(X[0, idxes_S1], X[1, idxes_S1], c = "blue", marker = 'v', s = 100)
-plt.scatter(X[0, idxes_S2], X[1, idxes_S2], c = "red", marker = 'v', s = 100)
+    plt.scatter(X[0, sslm.idxes_SVp], X[1, sslm.idxes_SVp], c = "blue", marker = 'v', s = 100)
+    plt.scatter(X[0, sslm.idxes_SVn], X[1, sslm.idxes_SVn], c = "red", marker = 'v', s = 100)
 
-plt.show()
+    plt.show()
+
 
