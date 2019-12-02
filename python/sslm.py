3eac9798259938621783df12e475a80ac9a11671
HiroIshida <spitfire.docomo@gmail.com>
2019-12-03 01:25:54 +0900

    plot sslm

--- python/sslm.py
+++ python/sslm.py
@@ -7,9 +7,39 @@ import cvxopt
 import math
 rn.seed(5)
 
+def show2d(func, bmin, bmax, N = 20, fax = None, levels = None):
+    # fax is (fix, ax)
+    # func: np.array([x1, x2]) list -> scalar
+    # func cbar specifies the same height curve
+    if fax is None:
+        fig, ax = plt.subplots() 
+    else:
+        fig = fax[0]
+        ax = fax[1]
+
+    mat_contour_ = np.zeros((N, N))
+    mat_contourf_ = np.zeros((N, N))
+    x1_lin, x2_lin = [np.linspace(bmin[i], bmax[i], N) for i in range(bmin.size)]
+    for i in range(N):
+        for j in range(N):
+            x = np.array([x1_lin[i], x2_lin[j]])
+            val_c, val_cf = func(x)
+            mat_contour_[i, j] = val_c
+            mat_contourf_[i, j] = val_cf
+    mat_contour = mat_contour_.T
+    mat_contourf = mat_contourf_.T
+    X, Y = np.meshgrid(x1_lin, x2_lin)
+
+    cs = ax.contour(X, Y, mat_contour, levels = levels, cmap = 'jet')
+    zc = cs.collections[0]
+    plt.setp(zc, linewidth=4)
+    ax.clabel(cs, fontsize=10)
+    cf = ax.contourf(X, Y, mat_contourf, cmap = 'gray_r')
+    fig.colorbar(cf)
+
 
 def gen_dataset_(N):
-    predicate = lambda x: x[0]**2 + x[1]**2 < 0.5
+    predicate = lambda x: x[0]**2 + x[1]**2 < 0.5 ** 2
     xp_lst = []
     xm_lst = []
     for i in range(N):
@@ -46,7 +76,7 @@ def gen_diadig(vec):
     return ymat
 
 
-N = 100
+N = 60
 #X, y, n_p = gen_dataset()
 X, y, n_p = gen_dataset_(N)
 kern = linear_kernel
@@ -118,11 +148,22 @@ rho = math.sqrt(P2/n2 - P1/n1)
 
 Xt = X.T
 x_new = Xt[1, :]
-predict = lambda x: (np.sign(R**2 - cc - kern(x, x) + sum(2 * kern(X.T, x).flatten() * a_lst * y))).item()
+predict = lambda x: (R**2 - cc - kern(x, x) + sum(2 * kern(X.T, x).flatten() * a_lst * y)).item()
 predict([0, 0.8])
 
 
 ## plot
+
+fig, ax = plt.subplots() 
+
+
+bmin = np.array([-1, -1])
+bmax = np.array([1, 1])
+def f(x):
+    tmp = predict(x)
+    return tmp, tmp
+show2d(f, bmin, bmax, fax = (fig, ax))
+
 idx_positive = np.where(y == 1)
 idx_negative = np.where(y == -1)
 
