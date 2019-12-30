5648f43c6e3babc934a308686e272beb424f5f59
HiroIshida <spitfire.docomo@gmail.com>
2019-12-30 16:38:25 +0900

    ordered grid gen

--- julia/progressive_grid.jl
+++ julia/progressive_grid.jl
@@ -1,4 +1,13 @@
-function grid_gen(n_depth)
+
+function ordered_grid_gen(n_depth)
+  x_lst = []
+  for i in 1:n_depth
+    append!(x_lst, progressive_grid_gen(i))
+  end
+  return x_lst
+end
+
+function progressive_grid_gen(n_depth)
 
   N = 2^(n_depth-1)+1
   x_lst_pre = []
@@ -19,8 +28,6 @@ function grid_gen(n_depth)
   return x_lst
 end
 
-
-
 using Plots
 function convert(x_lst)
   n = length(x_lst)
@@ -29,8 +36,13 @@ function convert(x_lst)
   return hcat(X1, X2)
 end
 
-x_lst_lst = [convert(grid_gen(i)) for i in 1:4]
+a1 = progressive_grid_gen(1)
+a2 = progressive_grid_gen(2)
+
+x_lst_lst = [convert(progressive_grid_gen(i)) for i in 1:4]
 scatter(x_lst_lst[1][:, 1], x_lst_lst[1][:, 2], color = "red")
 scatter!(x_lst_lst[2][:, 1], x_lst_lst[2][:, 2], color = "blue")
 scatter!(x_lst_lst[3][:, 1], x_lst_lst[3][:, 2], color = "green")
 scatter!(x_lst_lst[4][:, 1], x_lst_lst[4][:, 2], color = "yellow")
+
+ordered_grid_gen(4)
