28997dbc1a6288b8588555ce80cfffb224685e75
HiroIshida <spitfire.docomo@gmail.com>
2020-02-05 02:12:56 +0900

    cleaned

--- python/gpytorch/test2d.py
+++ python/gpytorch/test2d.py
@@ -17,9 +17,9 @@ def franke(X, Y):
     dfy = -2*(9*Y - 2)*9/4 * term1 - 9/10 * term2 + \
           -2*(9*Y - 3)*9/4 * term3 + 2*(9*Y - 7)*9 * term4
 
-    return f, dfx, dfy
+    return f
 
-N = 10
+N = 30
 xv, yv = torch.meshgrid([torch.linspace(0, 1, N), torch.linspace(0, 1, N)])
 train_x = torch.cat((
     xv.contiguous().view(xv.numel(), 1),
@@ -27,25 +27,27 @@ train_x = torch.cat((
     dim=1
 )
 
-f, dfx, dfy = franke(train_x[:, 0], train_x[:, 1])
-train_y = torch.stack([f, dfx, dfy], -1).squeeze(1)
+f = franke(train_x[:, 0], train_x[:, 1])
+train_y = torch.stack([f], -1).squeeze(1)
 
 train_y += 0.05 * torch.randn(train_y.size()) # Add noise to both values and gradients
 
-class GPModelWithDerivatives(gpytorch.models.ExactGP):
+# We will use the simplest form of GP model, exact inference
+class ExactGPModel(gpytorch.models.ExactGP):
     def __init__(self, train_x, train_y, likelihood):
-        super(GPModelWithDerivatives, self).__init__(train_x, train_y, likelihood)
-        self.mean_module = gpytorch.means.ConstantMeanGrad()
-        self.base_kernel = gpytorch.kernels.RBFKernelGrad(ard_num_dims=2)
-        self.covar_module = gpytorch.kernels.ScaleKernel(self.base_kernel)
-
+        super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
+        self.mean_module = gpytorch.means.ConstantMean()
+        self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
+    
     def forward(self, x):
         mean_x = self.mean_module(x)
         covar_x = self.covar_module(x)
-        return gpytorch.distributions.MultitaskMultivariateNormal(mean_x, covar_x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 
-likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(num_tasks=3)  # Value + x-derivative + y-derivative
-model = GPModelWithDerivatives(train_x, train_y, likelihood)
+#likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(num_tasks=3)  # Value + x-derivative + y-derivative
+likelihood = gpytorch.likelihoods.GaussianLikelihood()
+#model = GPModelWithDerivatives(train_x, train_y, likelihood)
+model = ExactGPModel(train_x, train_y, likelihood)
 
 # this is for running the notebook in our testing framework
 import os
@@ -71,12 +73,15 @@ for i in range(training_iter):
     output = model(train_x)
     loss = -mll(output, train_y)
     loss.backward()
+    '''
     print("Iter %d/%d - Loss: %.3f   lengthscales: %.3f, %.3f   noise: %.3f" % (
         i + 1, training_iter, loss.item(),
+        model.covar_module.base_kernel.lengthscale.item(),
         model.covar_module.base_kernel.lengthscale.squeeze()[0],
         model.covar_module.base_kernel.lengthscale.squeeze()[1],
         model.likelihood.noise.item()
     ))
+    '''
     optimizer.step()
 
 # Set into eval mode
@@ -89,7 +94,6 @@ fig, ax = plt.subplots(2, 3, figsize=(14, 10))
 # Test points
 n1, n2 = 50, 50
 xv, yv = torch.meshgrid([torch.linspace(0, 1, n1), torch.linspace(0, 1, n2)])
-f, dfx, dfy = franke(xv, yv)
 
 # Make predictions
 with torch.no_grad(), gpytorch.settings.fast_computations(log_prob=False, covar_root_decomposition=False):
@@ -98,19 +102,7 @@ with torch.no_grad(), gpytorch.settings.fast_computations(log_prob=False, covar_
     mean = predictions.mean
 
 extent = (xv.min(), xv.max(), yv.max(), yv.min())
-ax[0, 0].imshow(f, extent=extent, cmap=cm.jet)
-ax[0, 0].set_title('True values')
-ax[0, 1].imshow(dfx, extent=extent, cmap=cm.jet)
-ax[0, 1].set_title('True x-derivatives')
-ax[0, 2].imshow(dfy, extent=extent, cmap=cm.jet)
-ax[0, 2].set_title('True y-derivatives')
-
-ax[1, 0].imshow(mean[:, 0].detach().numpy().reshape(n1, n2), extent=extent, cmap=cm.jet)
-ax[1, 0].set_title('Predicted values')
-ax[1, 1].imshow(mean[:, 1].detach().numpy().reshape(n1, n2), extent=extent, cmap=cm.jet)
-ax[1, 1].set_title('Predicted x-derivatives')
-ax[1, 2].imshow(mean[:, 2].detach().numpy().reshape(n1, n2), extent=extent, cmap=cm.jet)
-ax[1, 2].set_title('Predicted y-derivatives')
+ax[1, 0].imshow(mean.detach().numpy().reshape(n1, n2), extent=extent, cmap=cm.jet)
 plt.show()
 
 None
