491a7caeb7375d17871c2d8a71bbfa66e75b2f09
HiroIshida <spitfire.docomo@gmail.com>
2020-02-05 03:10:19 +0900

    put optimize into class

--- python/gpytorch/test2d.py
+++ python/gpytorch/test2d.py
@@ -19,7 +19,7 @@ def franke(X, Y):
 
     return f
 
-N = 30
+N = 10
 xv, yv = torch.meshgrid([torch.linspace(0, 1, N), torch.linspace(0, 1, N)])
 train_x = torch.cat((
     xv.contiguous().view(xv.numel(), 1),
@@ -38,58 +38,39 @@ class ExactGPModel(gpytorch.models.ExactGP):
         super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
         self.mean_module = gpytorch.means.ConstantMean()
         self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
+        self.likelihood = likelihood
     
     def forward(self, x):
         mean_x = self.mean_module(x)
         covar_x = self.covar_module(x)
         return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 
-#likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(num_tasks=3)  # Value + x-derivative + y-derivative
+    def optimize(self):
+        self.train()
+        self.likelihood.train()
+        optimizer = torch.optim.Adam([
+            {'params': self.parameters()},  # Includes GaussianLikelihood parameters
+        ], lr=0.05)
+        mll = gpytorch.mlls.ExactMarginalLogLikelihood(self.likelihood, self)
+
+        for i in range(200):
+            print("hoge")
+            optimizer.zero_grad()
+            output = self(train_x)
+            loss = -mll(output, train_y)
+            loss.backward()
+            optimizer.step()
+
+        # switch to eval mode
+        self.eval()
+        self.likelihood.eval()
+
 likelihood = gpytorch.likelihoods.GaussianLikelihood()
-#model = GPModelWithDerivatives(train_x, train_y, likelihood)
 model = ExactGPModel(train_x, train_y, likelihood)
-
-# this is for running the notebook in our testing framework
-import os
-smoke_test = ('CI' in os.environ)
-training_iter = 2 if smoke_test else 50
-
-
-# Find optimal model hyperparameters
-model.train()
-likelihood.train()
-
-
-# Use the adam optimizer
-optimizer = torch.optim.Adam([
-    {'params': model.parameters()},  # Includes GaussianLikelihood parameters
-], lr=0.05)
-
-# "Loss" for GPs - the marginal log likelihood
-mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
-
-for i in range(training_iter):
-    optimizer.zero_grad()
-    output = model(train_x)
-    loss = -mll(output, train_y)
-    loss.backward()
-    '''
-    print("Iter %d/%d - Loss: %.3f   lengthscales: %.3f, %.3f   noise: %.3f" % (
-        i + 1, training_iter, loss.item(),
-        model.covar_module.base_kernel.lengthscale.item(),
-        model.covar_module.base_kernel.lengthscale.squeeze()[0],
-        model.covar_module.base_kernel.lengthscale.squeeze()[1],
-        model.likelihood.noise.item()
-    ))
-    '''
-    optimizer.step()
-
-# Set into eval mode
-model.eval()
-likelihood.eval()
+model.optimize()
 
 # Initialize plots
-fig, ax = plt.subplots(2, 3, figsize=(14, 10))
+fig, ax = plt.subplots(1, 1, figsize=(14, 10))
 
 # Test points
 n1, n2 = 50, 50
@@ -102,7 +83,7 @@ with torch.no_grad(), gpytorch.settings.fast_computations(log_prob=False, covar_
     mean = predictions.mean
 
 extent = (xv.min(), xv.max(), yv.max(), yv.min())
-ax[1, 0].imshow(mean.detach().numpy().reshape(n1, n2), extent=extent, cmap=cm.jet)
+ax.imshow(mean.detach().numpy().reshape(n1, n2), extent=extent, cmap=cm.jet)
 plt.show()
 
 None
