77706ba33266f95bc48b21e5ae78298c83e3dd14
HiroIshida <spitfire.docomo@gmail.com>
2020-02-05 04:42:06 +0900

    simplest example

--- python/gpytorch/test2d.py
+++ python/gpytorch/test2d.py
@@ -26,16 +26,15 @@ class ExactGPModel(gpytorch.models.ExactGP):
         ], lr=0.05)
         mll = gpytorch.mlls.ExactMarginalLogLikelihood(self.likelihood, self)
 
-        try:
-            for i in range(10):
-                print("hoge")
-                optimizer.zero_grad()
-                output = self(train_x)
-                loss = -mll(output, train_y)
-                loss.backward()
-                optimizer.step()
-        except RuntimeError:
-            print("cannot be optimized, more training data is necessary pribably")
+        for i in range(100):
+            print("hoge")
+            optimizer.zero_grad()
+            output = self(train_x)
+            loss = -mll(output, train_y)
+            loss.backward()
+            optimizer.step()
+        #except RuntimeError:
+            #print("cannot be optimized, more training data is necessary pribably")
 
         # switch to eval mode
         self.eval()
@@ -48,44 +47,26 @@ class ExactGPModel(gpytorch.models.ExactGP):
 
 
 
-import torch as th
-#X_init = th.tensor([[0.5, 0.3], [0.5, 0.5], [0.5, 0.7]])
-#Y_init = th.tensor([1, -1, 1])
-def gen_dataset(N):
-    predicate = lambda x: (x[0]**2 -0.5)**2 + (x[1]**2 -0.5)**2 < 0.4 ** 2
-    X = []
-    Y = []
-    for i in range(N):
-        x = th.rand(2)  
-        y = predicate(x) * 2 - 1
-        X.append(x)
-        Y.append(y)
-    X_ = th.stack(X, dim=0)
-    Y_ = th.stack(Y)
-    return X_, Y_
-X_init, Y_init = gen_dataset(30)
+if __name__=='__main__':
+    import torch as th
+    train_x = th.tensor([[0.5, 0.2], [0.5, 0.5], [0.5, 0.9]])
+    train_y = torch.tensor([-1, 1, -1])
 
+    likelihood = gpytorch.likelihoods.GaussianLikelihood()
+    model = ExactGPModel(train_x, train_y, likelihood)
+    model.optimize()
 
-likelihood = gpytorch.likelihoods.GaussianLikelihood()
-model = ExactGPModel(X_init, Y_init, likelihood)
-model.optimize()
+    fig, ax = plt.subplots(1, 1, figsize=(14, 10))
+    n1, n2 = 50, 50
+    xv, yv = torch.meshgrid([torch.linspace(0, 1, n1), torch.linspace(0, 1, n2)])
 
-#model.predict(torch.tensor([[0.5, 0.5]]))
+    # Make predictions
+    with torch.no_grad(), gpytorch.settings.fast_computations(log_prob=False, covar_root_decomposition=False):
+        test_x = torch.stack([xv.reshape(n1*n2, 1), yv.reshape(n1*n2, 1)], -1).squeeze(1)
+        predictions = likelihood(model(test_x))
+        mean = predictions.mean
 
-# Initialize plots
-fig, ax = plt.subplots(1, 1, figsize=(14, 10))
-
-# Test points
-n1, n2 = 50, 50
-xv, yv = torch.meshgrid([torch.linspace(0, 1, n1), torch.linspace(0, 1, n2)])
-
-# Make predictions
-with torch.no_grad(), gpytorch.settings.fast_computations(log_prob=False, covar_root_decomposition=False):
-    test_x = torch.stack([xv.reshape(n1*n2, 1), yv.reshape(n1*n2, 1)], -1).squeeze(1)
-    predictions = likelihood(model(test_x))
-    mean = predictions.mean
-
-extent = (xv.min(), xv.max(), yv.max(), yv.min())
-ax.imshow(mean.detach().numpy().reshape(n1, n2), extent=extent, cmap=cm.jet)
-plt.show()
+    extent = (xv.min(), xv.max(), yv.max(), yv.min())
+    ax.imshow(mean.detach().numpy().reshape(n1, n2), extent=extent, cmap=cm.jet)
+    plt.show()
 
