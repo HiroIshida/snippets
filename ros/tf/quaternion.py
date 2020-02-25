07f259893925cd4e803d3541d15310c264509aae
HiroIshida <spitfire.docomo@gmail.com>
2020-02-25 23:46:49 +0900

    check quaternion converter

--- ros/tf/quaternion.py
+++ ros/tf/quaternion.py
@@ -10,13 +10,14 @@ from geometry_msgs.msg import Quaternion
 rospy.init_node('commander')
 listener = tf.TransformListener()
 # https://answers.ros.org/question/196149/how-to-rotate-vector-by-quaternion-in-python/
+# http://docs.ros.org/jade/api/tf/html/python/transformations.html
 
 def test():
     while True:
         try:
-            tf1 = listener.lookupTransform('/base_link', '/oven', rospy.Time(0))
-            tf2 = listener.lookupTransform('/oven', '/handle', rospy.Time(0))
-            tf3 = listener.lookupTransform('/base_link', '/handle', rospy.Time(0))
+            tf1 = listener.lookupTransform('/base_link', '/l_shoulder_pan_link', rospy.Time(0))
+            tf2 = listener.lookupTransform('/l_shoulder_pan_link', '/l_upper_arm_link', rospy.Time(0))
+            tf3 = listener.lookupTransform('/base_link', '/l_upper_arm_link', rospy.Time(0))
             return tf1, tf2, tf3
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             continue
@@ -41,21 +42,10 @@ def convert(tf_body_to_oven, tf_oven_to_handle):
 
     rot_bh = tf.transformations.quaternion_multiply(rot_bo, rot_oh)
     tran_bh = tran_bo + qv_mult(rot_bo, tran_oh)
-    return list(tran_bh), list(tran_bo)
-
-
+    return list(tran_bh), list(rot_bh)
 
 if __name__=='__main__':
     tf1, tf2, tf3 = test()
-    #tran, rot = convert(tf1, tf2)
+    tran, rot = convert(tf1, tf2)
+    tran3, rot3 = tf3
 
-    trans1, rot1 = [np.array(e) for e in tf1]
-    trans2, rot2 = [np.array(e) for e in tf2]
-    trans3, rot3 = [np.array(e) for e in tf3]
-
-    tmp = tf.transformations.quaternion_multiply(rot1, rot2) 
-    print(tmp - rot3)
-
-    v = qv_mult(rot1, trans2)
-    print(trans1 + qv_mult(rot1, trans2))
-    print(trans3)
