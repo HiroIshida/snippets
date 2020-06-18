1dce27fc91d944e5467888a5c202f9d0f49e4736
HiroIshida <spitfire.docomo@gmail.com>
2020-06-19 06:41:22 +0900

    grayscale

---
+++ linux/pdf_grayscale.sh
@@ -0,0 +1,11 @@
+gs \
+ -sOutputFile=gray.pdf \
+ -sDEVICE=pdfwrite \
+ -sColorConversionStrategy=Gray \
+ -dProcessColorModel=/DeviceGray \
+ -dCompatibilityLevel=1.4 \
+ -dNOPAUSE \
+ -dBATCH \
+ dc.pdf
+rm dc.pdf
+
