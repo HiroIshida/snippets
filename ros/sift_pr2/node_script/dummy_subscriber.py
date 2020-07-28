5c83e41620c5b87fa81a94946b6604905f0b169b
HiroIshida <spitfire.docomo@gmail.com>
2020-07-28 14:36:32 +0900

    rosbag record and replay succeed

--- ros/sift_pr2/node_script/dummy_subscriber.py
+++ ros/sift_pr2/node_script/dummy_subscriber.py
@@ -2,15 +2,15 @@
 import rospy
 from posedetection_msgs.msg import ObjectDetection
 
-#topic_name = "/kinect_head/rgb/ObjectDetection"
-topic_name = "/kinect_head/depth_registered/ObjectDetection"
+prefix = "/head_camera_remote/"
+topic_name = prefix + "rgb/ObjectDetection"
 topic_type = ObjectDetection
 
 def callback(msg):
-    rospy.loginfo("subscribing objectDetection")
-    print("hoge")
+    #rospy.loginfo("subscribing objectDetection")
     pass
 
 rospy.init_node('dummy_subscriber')
 rospy.Subscriber(topic_name, topic_type, callback)
 rospy.spin()
+
