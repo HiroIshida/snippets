86fbf07d439f247564670e0f954852c2e34b3edd
HiroIshida <spitfire.docomo@gmail.com>
2020-07-28 19:59:09 +0900

    estimation

---
+++ ros/sift_pr2/preparation/record_estimation.sh
@@ -0,0 +1,1 @@
+rosbag record /kinect_head/rgb/ObjectDetection /kinect_head/rgb/image_rect_color /kinect_head/rgb/fridge_detector/debug_image /kinect_head/depth_registered/points /tf -O estimation.bag
