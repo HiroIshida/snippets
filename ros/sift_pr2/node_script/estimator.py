d962529224a9419a5656e4876ef280c3007dcf6d
HiroIshida <spitfire.docomo@gmail.com>
2020-07-29 09:51:08 +0900

    particle filter update

--- ros/sift_pr2/node_script/estimator.py
+++ ros/sift_pr2/node_script/estimator.py
@@ -3,6 +3,7 @@ import rospy
 import tf
 import numpy as np
 from posedetection_msgs.msg import ObjectDetection
+from particle_filter import ParticleFilter
 
 rospy.init_node("pose3d_estimator", anonymous=True)
 listener = tf.TransformListener()
@@ -19,7 +20,6 @@ def qv_mult(q1, v1_):
     )[:3]
     return v_converted * length
 
-
 def convert(tf_12, tf_23):
     tran_12, rot_12 = [np.array(e) for e in tf_12]
     tran_23, rot_23 = [np.array(e) for e in tf_23]
@@ -28,26 +28,40 @@ def convert(tf_12, tf_23):
     tran_13 = tran_23 + qv_mult(rot_23, tran_12)
     return list(tran_13), list(rot_13)
 
-def cb_object_detection(msg):
-    def convert_pose2tf(pose):
-        pos = pose.position
-        rot = pose.orientation
-        trans = [pos.x, pos.y, pos.z]
-        rot = [rot.x, rot.y, rot.z, rot.w]
-        return (trans, rot)
+class PoseEstimater:
+    def __init__(self, N):
+        self.N = N
+        self.pf = ParticleFilter(N)
+        self.sub = rospy.Subscriber("/kinect_head/rgb/ObjectDetection", 
+                ObjectDetection, self.cb_object_detection, queue_size=10)
 
-    header = msg.header
-    assert len(msg.objects) == 1
-    obj = msg.objects[0]
-    tf_handle_to_kinect = convert_pose2tf(obj.pose)
-    tf_kinect_to_map = listener.lookupTransform('/map', '/head_mount_kinect_rgb_optical_frame', rospy.Time(0))
-    tf_handle_to_map = convert(tf_handle_to_kinect, tf_kinect_to_map)
-    print(tf_handle_to_map)
+    def cb_object_detection(self, msg):
+        def convert_pose2tf(pose):
+            pos = pose.position
+            rot = pose.orientation
+            trans = [pos.x, pos.y, pos.z]
+            rot = [rot.x, rot.y, rot.z, rot.w]
+            return (trans, rot)
 
-sub = rospy.Subscriber("/kinect_head/rgb/ObjectDetection", 
-        ObjectDetection, 
-        cb_object_detection,
-        queue_size=10)
+        header = msg.header
+        assert len(msg.objects) == 1
+        obj = msg.objects[0]
+        tf_handle_to_kinect = convert_pose2tf(obj.pose)
+        tf_kinect_to_map = listener.lookupTransform('/map', '/head_mount_kinect_rgb_optical_frame', rospy.Time(0))
+        tf_handle_to_map = convert(tf_handle_to_kinect, tf_kinect_to_map)
+        trans = tf_handle_to_map[0]
+        rpy = tf.transformations.euler_from_quaternion(tf_handle_to_map[1])
+
+        state = np.array([trans[0], trans[1], rpy[1]])
+        cov = np.diag([0.2, 0.2, 0.1])
+        if self.pf.X is None:
+            ptcls = np.random.multivariate_normal(state, cov, self.N)
+            self.pf.initialize(ptcls)
+        else:
+            self.pf.update(state, cov)
+
+
+PoseEstimater(5000)
 rospy.spin()
 
 
---
+++ ros/sift_pr2/node_script/particle_filter.py
@@ -0,0 +1,129 @@
+import scipy.stats
+import numpy as np
+import math
+
+def marging_resampling(ptcls, w, mp = 0.9, 
+        unit_circle_idxes = [2],
+        unit_circle_bminmaxes = [[0, 2*3.1415]],
+        cython=False
+        ):
+    N = len(w)
+
+    sampler = low_variance_sampler
+    idxes1 = sampler(w)
+    idxes2_ = sampler(w)
+    idxes3_ = sampler(w)
+    perm2 = np.random.permutation(N)
+    perm3 = np.random.permutation(N)
+    idxes2 = idxes2_[perm2]
+    idxes3 = idxes3_[perm3]
+    a1 = mp
+    a2 = (0.5 * ((1 - a1) + math.sqrt(-3 * a1**2 + 2 * a1 + 1)))
+    a3 = (0.5 * ((1 - a1) - math.sqrt(-3 * a1**2 + 2 * a1 + 1)))
+    ptcl_tmp1 = ptcls[idxes1, :]
+    ptcl_tmp2 = ptcls[idxes2, :]
+    ptcl_tmp3 = ptcls[idxes3, :]
+    ptcls = ptcl_tmp1 * a1 + ptcl_tmp2 * a2 + ptcl_tmp3 * a3
+
+    N_unit_circle_merge = len(unit_circle_idxes)
+    for i in range(N_unit_circle_merge):
+        axis = unit_circle_idxes[i]
+        ptcl_partial_tmp1 = ptcl_tmp1[:, axis]
+        ptcl_partial_tmp2 = ptcl_tmp2[:, axis]
+        ptcl_partial_tmp3 = ptcl_tmp3[:, axis]
+
+        idx_ucm = unit_circle_idxes[i]
+        bmin_ucm = unit_circle_bminmaxes[i][0]
+        bmax_ucm = unit_circle_bminmaxes[i][1]
+        ptcls[:, axis] = unit_circle_merge(
+                [ptcl_partial_tmp1, ptcl_partial_tmp2, ptcl_partial_tmp3],
+                [a1, a2, a3], bmin_ucm, bmax_ucm)
+    return ptcls
+
+def low_variance_sampler(ws):
+    # ws is numpya array
+    N = len(ws)
+    idxes = np.zeros(N, dtype=int)
+    w_sum = sum(ws)
+    r = np.random.random()*(1.0/N)
+    c = ws[0]/w_sum
+    k = 0
+    for n in range(N):
+        U = r + n*(1.0/N)
+        while U > c:
+            k+=1
+            c = c+ws[k]/w_sum
+        idxes[n] = k
+    return idxes
+
+def unit_circle_encode(s, bmin, bmax):
+    tmp = (s - bmin) / (bmax - bmin)
+    phase = tmp * 2 * math.pi
+    x = np.vectorize(math.cos)(phase)
+    y = np.vectorize(math.sin)(phase)
+    return np.vstack((x, y))
+
+def stick_to_unit_circle(pos):
+    r = np.vectorize(math.sqrt)(np.sum(pos**2, 0))
+    return pos * (1/r)
+
+def unit_circle_decode(pos, bmin, bmax):
+    phase  = np.arctan2(pos[1], pos[0]) 
+    idx = np.where(phase < 0)[0]
+    phase[idx] =  phase[idx] + 2 * math.pi
+    s = (phase / (2 * math.pi)) * (bmax - bmin) + bmin
+    return s
+
+def regularize(angles, bmin, bmax):
+    tmp = (angles - bmin)/(bmax - bmin)
+    tmp2 = tmp - np.floor(tmp)
+    return tmp2 * (bmax - bmin) + bmin
+
+def unit_circle_merge(s_lst, a_lst, bmin, bmax):
+    pos0 = unit_circle_encode(s_lst[0], bmin, bmax)
+    pos1 = unit_circle_encode(s_lst[1], bmin, bmax)
+    pos2 = unit_circle_encode(s_lst[2], bmin, bmax)
+    pos_tmp = a_lst[0] * pos0 + a_lst[1] * pos1 + a_lst[2] * pos2
+    pos = stick_to_unit_circle(pos_tmp)
+    s = unit_circle_decode(pos, bmin, bmax)
+    return s
+
+class ParticleFilter:
+    def __init__(self, N, M=3):
+        self.N = N
+        self.M = M
+        self.X = None
+        self.W = None
+
+    def isInitialized(self):
+        return (self.X is not None)
+
+    def initialize(self, X, W=None):
+        N = X.shape[0]
+        self.X = X
+        self.X[:, 2] = regularize(X[:, 2], 0, 2*3.1415)
+        W = np.ones(N)/self.N
+        self.W = W
+
+    def default_likelihood_function(self, X, z, R):
+        Rinv = np.linalg.inv(R)
+        Rdet = np.linalg.det(R)
+        d = X - z.reshape(1, self.M).repeat(self.N, axis=0)
+        tmp = np.einsum('ij,jk', d, Rinv) 
+        dists = np.sum(tmp.T * d.T, axis=0)
+        return np.exp(-0.5 * dists)
+
+    def update(self, z, R, resampling=True, ess_threshold=0.4):
+        weights_likeli = self.default_likelihood_function(self.X, z, R)
+        tmp = self.W * weights_likeli
+        self.W = tmp/sum(tmp) 
+
+        ess = 1.0/(np.sum(self.W ** 2) * self.N) # effective sample size
+        print(ess)
+        if resampling:
+            if ess < ess_threshold:
+                message = "effective sample size is {0} < {1}, thus the resampling took place".format(\
+                        ess, ess_threshold)
+                print(message)
+                self.X = marging_resampling(self.X, self.W)
+                self.W = np.ones(self.N)/self.N
