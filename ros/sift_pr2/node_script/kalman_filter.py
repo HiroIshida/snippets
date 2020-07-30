d8f77c05f19b2f5b18f987bf7b86fb6445fe2af0
HiroIshida <spitfire.docomo@gmail.com>
2020-07-31 01:57:20 +0900

    outlier robust kalman

--- ros/sift_pr2/node_script/kalman_filter.py
+++ ros/sift_pr2/node_script/kalman_filter.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.stats as stats
 
 class KalmanFilter: # not really a kalman filter...
     def __init__(self, M):
@@ -14,13 +15,19 @@ class KalmanFilter: # not really a kalman filter...
 
     def update(self, z_obs, R):
         # notation is similar to wikipedia
-        y = z_obs - self.x_est # H = eye
-        S = self.P + R
-        K = self.P.dot(np.linalg.inv(S))
-        x_est_new = self.x_est + K.dot(y)
-        P_new = (np.eye(self.M) - K).dot(self.P)
-        self.x_est = x_est_new
-        self.P = P_new
+        if self.isValidObservation(z_obs):
+            y = z_obs - self.x_est # H = eye
+            S = self.P + R
+            K = self.P.dot(np.linalg.inv(S))
+            x_est_new = self.x_est + K.dot(y)
+            P_new = (np.eye(self.M) - K).dot(self.P)
+            self.x_est = x_est_new
+            self.P = P_new
+
+    def isValidObservation(self, z_obs):
+        prob = stats.multivariate_normal(self.x_est, self.P).pdf(z_obs)
+        print(prob)
+        return (prob > 1.0)
 
     def get_current_est(self, withCov=False):
         if withCov:
