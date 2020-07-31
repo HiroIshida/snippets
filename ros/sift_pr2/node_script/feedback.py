625a9654959e702e991df0afd23d31422a2659a5
HiroIshida <spitfire.docomo@gmail.com>
2020-07-31 15:41:24 +0900

    completed feedback fridge grasp

--- ros/sift_pr2/node_script/feedback.py
+++ ros/sift_pr2/node_script/feedback.py
@@ -18,35 +18,45 @@ def convert_posemsg_to_transform(msg):
     rot = [ori.x, ori.y, ori.z, ori.w] # quat
     return [trans, rot]
 
-rospy.init_node("feedback_robot_controller", anonymous=True)
-listener = tf.TransformListener()
 
-CLIENT = pybullet.connect(pybullet.GUI)
+CLIENT = pybullet.connect(pybullet.DIRECT)
 pb.configureDebugVisualizer(pybullet.COV_ENABLE_GUI, 0, physicsClientId=CLIENT)
 pb.configureDebugVisualizer(pybullet.COV_ENABLE_SHADOWS, 0, physicsClientId=CLIENT)
 pb.configureDebugVisualizer(pybullet.COV_ENABLE_TINY_RENDERER, 0, physicsClientId=CLIENT)
 robot = sb.robot_models.PR2(CLIENT)
-ri = PR2ROSRobotInterface(robot)
 
-
-
-def cb_fridge_pose(msg):
-    tf_handle_to_map = convert_posemsg_to_transform(msg)
-    tf_map_to_base = listener.lookupTransform("/base_link", "/map", rospy.Time(0))
-    tf_handle_to_base = convert(tf_handle_to_map, tf_map_to_base)
-
-    trans, quat = tf_handle_to_base
-    rpy = tf.transformations.euler_from_quaternion(quat)
-    robot.solve_ik(trans, rpy, group_names=('larm'))
-    ri.angle_vector(robot.get_angle_vector(), time=0.2)
-    print(trans)
-
-sub = rospy.Subscriber("fridge_pose", PoseStamped, cb_fridge_pose, queue_size=10)
-rospy.spin()
-
-"""
+x_target = [0.2, 0.5, 1.3]
+q_new = robot.solve_ik(x_target, group_names=('larm', 'torso'))
+x_target = [0.2, -0.5, 1.3]
 q_new = robot.solve_ik(x_target, group_names=('rarm', 'torso'))
-x_target = [0.3, 0.5, 1.7]
-q_new = robot.solve_ik(x_target, group_names=('larm'))
-"""
+
+hoge = robot.get_joint_ids_in_groups(None)
+
+use_ros = True
+
+
+if use_ros:
+    rospy.init_node("feedback_robot_controller", anonymous=True)
+    listener = tf.TransformListener()
+    ri = PR2ROSRobotInterface(robot)
+    ri.angle_vector(robot.get_angle_vector(), time=0.2)
+    def cb_fridge_pose(msg):
+        tf_handle_to_map = convert_posemsg_to_transform(msg)
+        tf_map_to_base = listener.lookupTransform("/base_link", "/map", rospy.Time(0))
+        tf_handle_to_base = convert(tf_handle_to_map, tf_map_to_base)
+
+        trans, quat = tf_handle_to_base
+        rpy = tf.transformations.euler_from_quaternion(quat)
+        robot.solve_ik(trans, rpy, group_names=('larm'))
+        ri.angle_vector(robot.get_angle_vector(), time=0.2)
+        print(trans)
+
+    sub = rospy.Subscriber("fridge_pose", PoseStamped, cb_fridge_pose, queue_size=10)
+    rospy.spin()
+
+    """
+    q_new = robot.solve_ik(x_target, group_names=('rarm', 'torso'))
+    x_target = [0.3, 0.5, 1.7]
+    q_new = robot.solve_ik(x_target, group_names=('larm'))
+    """
 
