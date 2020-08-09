8c247f03766915e481e7020144edef4e7203ae20
HiroIshida <spitfire.docomo@gmail.com>
2020-08-09 12:30:02 +0900

    mutable

--- cpp/const/sample.cpp
+++ cpp/const/sample.cpp
@@ -4,9 +4,9 @@ class Sample
 {
   public:
     Sample(int var_) : var(var_){}
-    std::map<int, int> cache;
+    mutable std::map<int, int> cache;
     int var;
-    int heavy_computation(int x) 
+    int heavy_computation(int x) const
     {
       std::map<int, int>::iterator iter = cache.find(x);
       if(iter == cache.end()){ // cache doesn't exist
