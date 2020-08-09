b9e465232600665f8d7d4b5681612fe7d5c70e21
HiroIshida <spitfire.docomo@gmail.com>
2020-08-09 12:09:56 +0900

    relaxed const example

---
+++ cpp/const/sample.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<map>
+class Sample
+{
+  public:
+    Sample(int var_) : var(var_){}
+    std::map<int, int> cache;
+    int var;
+    int heavy_computation(int x) 
+    {
+      std::map<int, int>::iterator iter = cache.find(x);
+      if(iter == cache.end()){ // cache doesn't exist
+        int y = var + x; 
+        cache[x] =y;
+        return var;
+      }
+      std::cout << "cached one" << std::endl; 
+      return iter->second;
+    }
+};
+
+int main(){
+  Sample s(0);
+  s.heavy_computation(1);
+  s.heavy_computation(1);
+}
