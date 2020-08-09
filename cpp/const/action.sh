a6d426c2fdcd9c06e594d44a396e012e63a54158
HiroIshida <spitfire.docomo@gmail.com>
2020-08-09 12:51:27 +0900

    access by at() when map is const

---
+++ cpp/const/action.sh
@@ -0,0 +1,2 @@
+g++ sample.cpp
+./a.out
--- cpp/const/sample.cpp
+++ cpp/const/sample.cpp
@@ -1,11 +1,27 @@
 #include<iostream>
 #include<map>
+/*
+ *  mutable keyward
+ *  use at() for map function when the map is const
+ */
 class Sample
 {
   public:
-    Sample(int var_) : var(var_){}
+    Sample(int var_) : var(var_){
+      for(int i=0; i<10; i++){
+        table[i] = 2 * i;
+      }
+    }
     mutable std::map<int, int> cache;
+    std::map<int, int> table;
     int var;
+
+    int access(int x) const
+    {
+      //int hoge = table[var]; compile error
+      int hoge = table.at(var);
+    }
+
     int heavy_computation(int x) const
     {
       std::map<int, int>::iterator iter = cache.find(x);
