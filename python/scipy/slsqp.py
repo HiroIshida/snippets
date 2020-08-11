2db0b992fd06fb103b038c0a05cc6cccc6cc6203
HiroIshida <spitfire.docomo@gmail.com>
2020-08-11 11:45:06 +0900

    scipy opt with closure

--- python/scipy/slsqp.py
+++ python/scipy/slsqp.py
@@ -4,17 +4,32 @@ from scipy.optimize import *
 global jac_cache
 jac_cache = None
 
-def ineq_fun(x):
-    fx = np.array([1 - x[0] - 2*x[1], 1 - x[0]**2 - x[1], 1 - x[0]**2 + x[1]])
-    global jac_cache
-    jac_cache = np.array([[-1.0, -2.0], [-2*x[0], -1.0], [-2*x[0], 1.0]])
-    return fx
+def naive_way_to_make_ineqfun():
+    def ineq_fun(x):
+        fx = np.array([1 - x[0] - 2*x[1], 1 - x[0]**2 - x[1], 1 - x[0]**2 + x[1]])
+        global jac_cache
+        jac_cache = np.array([[-1.0, -2.0], [-2*x[0], -1.0], [-2*x[0], 1.0]])
+        return fx
 
-def ineq_jac_fun(x):
-    global jac_cache
-    if jac_cache is None:
-        raise Exception
-    return jac_cache
+    def ineq_jac_fun(x):
+        global jac_cache
+        if jac_cache is None:
+            raise Exception
+        return jac_cache
+    return ineq_fun, ineq_jac_fun
+
+def generate_ineq_funs():
+    member = {'jac_cache': None}
+    ineq_jac_fun = lambda x: member['jac_cache']
+
+    def ineq_fun(x):
+        fx = np.array([1 - x[0] - 2*x[1], 1 - x[0]**2 - x[1], 1 - x[0]**2 + x[1]])
+        member['jac_cache'] = np.array([[-1.0, -2.0], [-2*x[0], -1.0], [-2*x[0], 1.0]])
+        return fx
+
+    return ineq_fun, ineq_jac_fun
+
+ineq_fun, ineq_jac_fun = generate_ineq_funs()
 
 ineq_cons = {'type': 'ineq',
              'fun' : ineq_fun,
