ab647d1e095c81ecf9e5ba7eaace6af19d76a8f9
HiroIshida <spitfire.docomo@gmail.com>
2020-08-18 09:53:04 +0900

    pickle

--- ros/occupancy_grid/occgrid.py
+++ ros/occupancy_grid/occgrid.py
@@ -6,6 +6,7 @@ import copy
 import StringIO
 import numpy as np
 import matplotlib.pyplot as plt 
+import pickle 
 
 def gridmsg2nparray(msg):
     info = msg.info
@@ -31,11 +32,18 @@ class MapManager:
     def show_map(self):
         plt.pcolor(self.arr)
 
+    def save_map(self, name="localcost_pr2.pickle"):
+        with open(name, 'wb') as f:
+            pickle.dump(self.arr, f)
+
+
 if __name__=='__main__':
-    rospy.init_node('wifi_publisher')
+    rospy.init_node('map_saver')
     mm = MapManager();
     r = rospy.Rate(10)
     for i in range(20):
         r.sleep()
     mm.show_map()
+    plt.show()
+    mm.save_map()
 
