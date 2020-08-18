0ebe9426e37a6b35878a30a028d9ba631a96a55d
HiroIshida <spitfire.docomo@gmail.com>
2020-08-18 17:38:20 +0900

    fin grid

--- ros/occupancy_grid/grid_itp.py
+++ ros/occupancy_grid/grid_itp.py
@@ -19,13 +19,13 @@ bmin = [0, 0]
 bmax = [nx * data.res, ny * data.res]
 xlin = np.linspace(bmin[0], bmax[0], 200)
 ylin = np.linspace(bmin[1], bmax[1], 200)
-f = scipy.interpolate.interp2d(xlin, ylin, np.fliplr(data.arr), kind='cubic')
-#fp = scipy.interpolate.RegularGridInterpolator((xlin, ylin), np.fliplr(data.arr), method='linear')
+finterp = scipy.interpolate.interp2d(xlin, ylin, np.fliplr(data.arr), kind='cubic')
+fp = scipy.interpolate.RegularGridInterpolator((xlin, ylin), np.fliplr(data.arr), method='linear')
 
 fig, ax = plt.subplots()
 X, Y = np.meshgrid(xlin, ylin)
-Z = f(xlin, ylin)
-ax.contourf(X, Y, Z)
+Z = finterp(xlin, ylin)
+ax.contourf(X, Y, Z.T)
 
 def base_to_map(P): # P|base -> P|map
     n_points = len(P)
@@ -37,9 +37,10 @@ def base_to_map(P): # P|base -> P|map
     points = points - np.repeat(mappos_origin, n_points, 0)
     return points
 
-#fs = fp(points)
-#idxes = fs < 30
-#ax.scatter(points[idxes, 0], points[idxes, 1]) 
+points = np.array(zip(X.flatten(), Y.flatten()))
+fs = fp(points)
+idxes = fs < 30
+ax.scatter(points[idxes, 0], points[idxes, 1]) 
 
 P_origin = np.array([[0.0, 0.0], [1.0, 0.0], [0.0, 1.0]])
 P_map = base_to_map(P_origin)
