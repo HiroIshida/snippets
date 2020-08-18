8dc76c92a5ccc8d85dbf575a5bd74339e7c47588
HiroIshida <spitfire.docomo@gmail.com>
2020-08-18 16:36:45 +0900

    tf

---
+++ ros/tf/simple.py
@@ -0,0 +1,13 @@
+import rospy
+import tf
+
+rospy.init_node('hoge')
+target_frame  = "/torso_lift_link"
+source_frame = "/base_footprint"
+listener = tf.TransformListener()
+listener.waitForTransform(target_frame, source_frame, rospy.Time(), rospy.Duration(4.0))
+tf = listener.lookupTransform(target_frame, source_frame, rospy.Time(0))
+print(tf)
+"""return
+([0.05, 0.0, -0.8588468872983683], [-0.0, -0.0, -0.0, 1.0])
+"""
