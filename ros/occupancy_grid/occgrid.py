3adc13b61f63ef0fc24d4f53a995d423ff182c0c
HiroIshida <spitfire.docomo@gmail.com>
2020-08-19 09:34:02 +0900

    minimum code demo

--- ros/occupancy_grid/occgrid.py
+++ ros/occupancy_grid/occgrid.py
@@ -9,13 +9,6 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import pickle 
 
-def gridmsg2nparray(msg):
-    info = msg.info
-    w = info.width
-    h = info.height
-    npnized = np.array(msg.data).reshape((w, h))
-    return npnized
-
 def generate_sdf(msg, tf_base_to_odom):
     info = msg.info
     n_grid = np.array([info.width, info.height])
@@ -45,13 +38,6 @@ def generate_sdf(msg, tf_base_to_odom):
     fp_wrt_base = lambda X: fp_wrt_map(base_to_map(X))
     return fp_wrt_base
 
-class MapData:
-    def __init__(self, arr, res, origin, tf_base_to_odom):
-        self.arr = arr
-        self.tf_base_to_odom = tf_base_to_odom
-        self.origin = origin
-        self.res = res
-
 class MapManager:
     def __init__(self):
         msg_name  = "/move_base_node/local_costmap/costmap"
@@ -59,16 +45,11 @@ class MapManager:
         self.listener = tf.TransformListener()
         self.msg = None
         self.arr = None
-        self.data = None
         self.costmapf = None
 
     def map_callback(self, msg):
         print("rec")
         self.msg = copy.deepcopy(msg)
-        info = msg.info
-        arr = gridmsg2nparray(msg)
-        self.arr = arr
-
         while(True):
             try:
                 tf_base_to_odom = self.listener.lookupTransform(msg.header.frame_id, "/base_footprint", rospy.Time(0))
@@ -76,9 +57,6 @@ class MapManager:
                 break
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
                 continue
-        print(info.origin)
-        self.data = MapData(arr, info.resolution, info.origin, tf_base_to_odom)
-
         self.costmapf = generate_sdf(msg, tf_base_to_odom)
 
     def show_map_wrtbase(self):
@@ -93,33 +71,6 @@ class MapManager:
         ax.contourf(X, Y, Z)
         plt.show()
 
-    def show_map(self):
-        nx, ny = self.data.arr.shape
-
-        bmin = [0, 0]
-        bmax = [nx * self.data.res, ny * self.data.res]
-        xlin = np.linspace(bmin[0], bmax[0], 200)
-        ylin = np.linspace(bmin[1], bmax[1], 200)
-
-        # SAVE THIS
-        # finterp = scipy.interpolate.interp2d(xlin, ylin, self.data.arr.T, kind='cubic')
-        # SAVE THIS
-
-        finterp = scipy.interpolate.interp2d(xlin, ylin, self.data.arr.T, kind='cubic')
-
-
-        fig, ax = plt.subplots()
-        X, Y = np.meshgrid(xlin, ylin)
-        Z = finterp(xlin, ylin)
-        ax.contourf(X, Y, Z.T)
-        plt.show()
-
-
-    def save_map(self, name="localcost_pr2.pickle"):
-        with open(name, 'wb') as f:
-            pickle.dump(self.data, f)
-
-
 if __name__=='__main__':
     rospy.init_node('map_saver')
     mm = MapManager();
@@ -127,5 +78,3 @@ if __name__=='__main__':
     for i in range(10):
         r.sleep()
     mm.show_map_wrtbase()
-    #mm.save_map()
-
