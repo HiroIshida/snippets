b11298a69194045003381af3625a69c80059712e
HiroIshida <spitfire.docomo@gmail.com>
2020-09-15 22:08:30 +0900

    implemented just following the paper, but it doesnt work at all!!! SO TIRED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! .

--- math/optimization/evo/nes.py
+++ math/optimization/evo/nes.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.linalg import sqrtm
+from scipy.linalg import sqrtm, expm
 
 def create_fax_unless_specifeid(fax):
     if fax is None:
@@ -14,10 +14,13 @@ class Rosen:
         pass
 
     def __call__(self, pts):
+        if pts.ndim == 1:
+            pts = np.array([pts])
         a = 2.0
         b = 100.0
         X, Y = pts[:, 0], pts[:, 1]
-        f = (a - X) ** 2 + b * (Y - X**2)**2
+        #f = (a - X) ** 2 + b * (Y - X**2)**2
+        f = X ** 2 + Y ** 2
         return f
 
     def show(self, fax=None):
@@ -29,52 +32,59 @@ class Rosen:
         pts = np.array(list(zip(X.flatten(), Y.flatten())))
         Z = self.__call__(pts).reshape((N, N))
         fig, ax = create_fax_unless_specifeid(fax)
-        ax.contourf(X, Y, Z, levels=[2**n for n in range(17)])
+        #ax.contourf(X, Y, Z, levels=[2**n for n in range(17)])
+        ax.contourf(X, Y, Z)
 
 class NaturalEvolution:
     def __init__(self, x_init):
         self.x_mean = x_init
-        self.cov = np.diag((1, 1)) 
-        self.A = np.linalg.cholesky(self.cov).T
-        self.lam = 10
         self.n_dim = len(x_init)
 
+        self.cov = np.diag((1, 1)) 
+        A = np.linalg.cholesky(self.cov).T
+        self.sigma = abs(np.linalg.det(A)) ** (1/self.n_dim)
+        self.B = A/self.sigma
+
+        #self.lam = int(4 + 3*np.log(self.n_dim)) 
+        self.lam = 1000
+        self.eta_sigma = 3*(3+np.log(self.n_dim))*(1.0/(5*self.n_dim*np.sqrt(self.n_dim))) 
+        self.eta_bmat = 3*(3+np.log(self.n_dim))*(1.0/(5*self.n_dim*np.sqrt(self.n_dim)))
+        self.eta_mu = 1.0
+
     def step(self, fun):
-        cov_inv = np.linalg.inv(self.cov)
+        s_lst = np.random.randn(self.lam, self.n_dim)
+        z_lst = self.x_mean + self.sigma * np.dot(s_lst, self.B)
+        f_lst = [fun(z).item() for z in z_lst]
 
-        x_rands = np.random.multivariate_normal(self.x_mean, self.cov, self.lam)
-        costs = fun(x_rands)
+        f_mean = np.mean(f_lst)
 
-        diffs = [(x_rand - self.x_mean).reshape(1, self.n_dim) for x_rand in x_rands]
-        nabla_x_lst = [diff.dot(cov_inv) for diff in diffs]
-        nabla_cov_lst = [0.5 * cov_inv.dot(np.outer(diff, diff)).dot(cov_inv) - 0.5 * cov_inv \
-                for diff in diffs]
+        rankebased = False
+        if rankebased:
+            idxes_upper = np.where(f_lst - f_mean < 0)[0]
+            n_nonzero = len(idxes_upper)
+            u_lst = 1.0/float(n_nonzero) * (f_lst - f_mean < 0) 
 
-        nabla_A_lst = [self.A.dot(nabla_cov + nabla_cov.T).reshape((1, self.n_dim**2)) for nabla_cov in nabla_cov_lst]
-        print(nabla_A_lst)
+        u_lst = 1.0/self.lam * np.ones(self.lam)
 
-        # making a big Phi matrix
-        left = np.vstack(diffs)
-        middle = np.vstack(nabla_A_lst)
-        right = np.ones((self.lam, 1))
-        Phi = np.hstack((left, middle, right))
-        print(np.linalg.matrix_rank(Phi))
+        nabla_delta = sum([u * s for u, s in zip(u_lst, s_lst)])
+        tmp_lst = [u * (np.outer(s, s) - np.eye(self.n_dim)) for s in s_lst]
+        nabla_M = sum(tmp_lst)
+        nabla_sigma = np.trace(nabla_M) / self.n_dim
+        nabla_B = nabla_M  - nabla_sigma * np.eye(self.n_dim)
 
-        Phi_psudo_inv = np.linalg.inv(Phi.T.dot(Phi)).dot(Phi.T)
-        #R = costs.reshape((self.lam, 1))
-
-#        d_theta = Phi_psudo_inv.dot(R)
-
-
-
-
-        
-
-
-
+        self.x_mean += self.eta_mu * self.sigma * nabla_delta.dot(self.B.T)
+        self.sigma *= np.exp(self.eta_sigma * 0.5 * nabla_sigma)
+        self.B = self.B.dot(expm(self.eta_bmat * 0.5 * nabla_B))
 
 fun = Rosen()
-nes = NaturalEvolution(np.ones(2))
-nes.step(fun)
+fun.show()
+#plt.show()
+nes = NaturalEvolution(np.array([-1, 1.0]))
+for i in range(1000):
+    nes.step(fun)
+    print(nes.x_mean)
+    print(nes.sigma)
+    #print(fun(nes.x_mean))
+
 #fun.show()
 #plt.show()
