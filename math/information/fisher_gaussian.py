c992d5a42f6ddbd9d7f15ab0fb0bc146fdc32c19
HiroIshida <spitfire.docomo@gmail.com>
2020-09-20 02:42:13 +0900

    compute fisher using MC

--- math/information/fisher_gaussian.py
+++ math/information/fisher_gaussian.py
@@ -1,5 +1,6 @@
 import numpy as np
 from copy import copy
+from scipy.stats import multivariate_normal
 
 def generate_pair(dim):
     for i in range(dim):
@@ -38,4 +39,46 @@ def compute(theta):
     I = symmetrize(I_tril) # fisher info mat
     print(I)
 
-compute([1, 0.0, 1])
+def cov(theta):
+    dim = calculate_dim(theta)
+    pair_lst = list(generate_pair(dim))
+    L = np.zeros((dim, dim))
+    for ((i, j), val) in zip(pair_lst, theta):
+        L[i, j] = val
+    M = L.dot(L.T)
+    return M
+
+def normpdf(x, theta):
+    M = cov(theta)
+    dim = M.shape[0]
+    return multivariate_normal.pdf(x, np.zeros(dim), M)
+
+def compute_grad(fun, x):
+    dim = len(x)
+    eps = 1e-3
+    f0 = fun(x)
+    grad = np.zeros(dim)
+    for i in range(dim):
+        x_ = copy(x)
+        x_[i] += eps
+        f1 = fun(x_)
+        grad[i] = (f1 - f0)/eps
+    return grad
+
+def compute_mc(theta):
+    dim = calculate_dim(theta)
+    pdf = lambda x: normpdf(x, theta)
+    M = cov(theta)
+
+    N_mc = 10000
+    X = np.random.multivariate_normal(np.zeros(dim), M, N_mc)
+
+    def rank1_mat(x):
+        fun = lambda theta: normpdf(x, theta)
+        vec = compute_grad(fun, theta)/pdf(x) # grad_loglikeli
+        return np.outer(vec, vec)
+    rank1_mat_lst = [rank1_mat(x) for x in X]
+    I_mc = sum(rank1_mat_lst) * (1.0/N_mc)
+    print(I_mc)
+
+compute_mc([1, 0.5, 1])
