64d71b6b86550c672137940cea6994bdae2d8381
HiroIshida <spitfire.docomo@gmail.com>
2020-09-23 22:58:42 +0900

    compare skimage vs pymcubes (pymcubes is 20x faster)

--- python/marching_cube/test_pymcubes.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import mcubes
-
-# Create a data volume (30 x 30 x 30)
-X, Y, Z = np.mgrid[:30, :30, :30]
-u = (X-15)**2 + (Y-15)**2 + (Z-15)**2 - 8**2
-
-# Extract the 0-isosurface
-vertices, triangles = mcubes.marching_cubes(u, 0)
