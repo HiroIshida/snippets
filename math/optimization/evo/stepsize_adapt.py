a6e114f69575059b45c77fc1e94d8ae4e2de06cf
HiroIshida <spitfire.docomo@gmail.com>
2020-10-21 01:29:54 +0900

    implemented adapative step

--- math/optimization/evo/stepsize_adapt.py
+++ math/optimization/evo/stepsize_adapt.py
@@ -17,11 +17,11 @@ class Rosen:
         b = 100.0
         X, Y = pts[:, 0], pts[:, 1]
         f = (a - X) ** 2 + b * (Y - X**2)**2
-        #f = X ** 2 + Y ** 2
+        f = X ** 2 + Y ** 2
         return f
 
     def show(self, fax=None):
-        N = 100
+        N = 20
         b_min = np.array([-3, -3])
         b_max = - b_min
         xlin, ylin = [np.linspace(b_min[i], b_max[i], N) for i in range(2)]
@@ -33,35 +33,42 @@ class Rosen:
         ax.contourf(X, Y, Z)
 
 class Optimizer(object):
-    def __init__(self, x_init, fun, sigma=0.5):
+    def __init__(self, x_init, fun, sigma=0.1):
         self.x = x_init
         self.n = len(x_init)
         self.sigma = sigma
         self.fun = fun
-        self.N_mc = 20
+        self.N_mc = 100
 
         # see hansen, ., auger 2015 evolution strategies Algorithm 4
         self.mu = int(self.N_mc * 0.25)
         self.d = 3 * self.n
-        self.s = 0 
-        self.c_sigma = np.sqrt(self.mu / ((self.n + self.mu) * 1.0))
+        self.s = np.zeros(self.n)
+        self.c_sigma = np.sqrt(self.mu / ((self.n + self.mu) * 1.0)) * 1
 
     def step(self):
-        x_rands = np.array([self.x + np.random.randn(self.n) * self.sigma 
-            for _ in range(self.N_mc)])
+        rands = np.random.randn(self.N_mc, self.n) # z
+        x_rands = np.array([self.x + rn * self.sigma for rn in rands])
         fs = self.fun(x_rands)
         idxes_elite = np.argsort(fs)[:self.mu]
         x_elites = x_rands[idxes_elite, :]
         self.x = np.mean(x_elites, axis=0)
 
+        coef =  np.sqrt(self.c_sigma * (2 - self.c_sigma)) * np.sqrt(self.mu)/self.mu
+        self.s = (1 - self.c_sigma) * self.s + coef * sum(rands[idxes_elite])
+        self.sigma = self.sigma * np.exp(np.abs(self.s) / 1.0 - 1)**(1.0/self.d) \
+                * np.exp(np.linalg.norm(self.s)/np.sqrt(self.n) - 1.0)**(self.c_sigma/self.d)
+
+
+
 func = Rosen()
 opt = Optimizer(np.array([5.0, 5.0]), func)
-path = []
-for i in range(300):
+path = [opt.x]
+for i in range(30):
     opt.step()
     path.append(opt.x)
 P = np.vstack(path)
 fig, ax = plt.subplots()
 func.show((fig, ax))
-ax.plot(P[:, 0], P[:, 1])
+ax.plot(P[:, 0], P[:, 1], "o-")
 plt.show()
