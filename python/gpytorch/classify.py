4825d2881624adbe2a40e2551e7449f309912e44
HiroIshida <spitfire.docomo@gmail.com>
2020-10-21 11:19:26 +0900

    updated gpytorch minimum example

--- python/gpytorch/classify.py
+++ python/gpytorch/classify.py
@@ -20,8 +20,8 @@ class GPClassificationModel(ApproximateGP):
         )
         super(GPClassificationModel, self).__init__(variational_strategy)
         self.mean_module = gpytorch.means.ConstantMean()
-        kern = gpytorch.kernels.RBFKernel()
-        kern.lengthscale = 0.2
+        kern = gpytorch.kernels.RBFKernel(ard_num_dims=2)
+        kern.lengthscale = torch.tensor([0.5, 0.5])
         self.covar_module = gpytorch.kernels.ScaleKernel(kern)
 
     def forward(self, x):
@@ -36,9 +36,6 @@ likelihood = gpytorch.likelihoods.BernoulliLikelihood()
 
 # this is for running the notebook in our testing framework
 import os
-smoke_test = ('CI' in os.environ)
-training_iterations = 2 if smoke_test else 50
-
 
 # Find optimal model hyperparameters
 model.train()
@@ -51,7 +48,7 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
 # num_data refers to the number of training datapoints
 mll = gpytorch.mlls.VariationalELBO(likelihood, model, train_y.numel())
 
-for i in range(200):
+for i in range(30):
     # Zero backpropped gradients from previous iteration
     optimizer.zero_grad()
     # Get predictive output
@@ -59,7 +56,6 @@ for i in range(200):
     # Calc loss and backprop gradients
     loss = -mll(output, train_y)
     loss.backward()
-    print('Iter %d/%d - Loss: %.3f' % (i + 1, training_iterations, loss.item()))
     optimizer.step()
 
 # Go into eval mode
@@ -67,16 +63,15 @@ model.eval()
 likelihood.eval()
 
 fig, ax = plt.subplots(1, 1, figsize=(14, 10))
-n1, n2 = 30, 30
-xv, yv = torch.meshgrid([torch.linspace(0, 1, n1), torch.linspace(0, 1, n2)])
+N = 100
+import numpy as np
+xlin = ylin = np.linspace(0, 1, N)
+X, Y = np.meshgrid(xlin, ylin)
+pts = torch.tensor([[x,y] for x, y in list(zip(X.flatten(), Y.flatten()))]).float()
 
 # Make predictions
 with torch.no_grad(), gpytorch.settings.fast_computations(log_prob=False, covar_root_decomposition=False):
-    test_x = torch.stack([xv.reshape(n1*n2, 1), yv.reshape(n1*n2, 1)], -1).squeeze(1)
-    predictions = likelihood(model(test_x))
-    mean = predictions.mean
-
-extent = (xv.min(), xv.max(), yv.max(), yv.min())
-ax.imshow(mean.detach().numpy().reshape(n1, n2), extent=extent, cmap=cm.jet)
+    predictions = likelihood(model(pts))
+    mean = predictions.mean.detach().numpy()
+ax.contourf(X, Y, mean.reshape((N, N)))
 plt.show()
-
