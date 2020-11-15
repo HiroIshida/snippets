fccd0d55b6c3e2119c6f693e67808a8b14984858
HiroIshida <spitfire.docomo@gmail.com>
2020-11-15 21:33:48 +0900

    use newcoords!

--- python/skrobot/move_robotbase.py
+++ python/skrobot/move_robotbase.py
@@ -1,18 +1,14 @@
 import skrobot
 import numpy as np
-from math import *
+from skrobot.coordinates.math import rpy_matrix
 from skrobot.coordinates import Coordinates
 
 robot_model = skrobot.models.urdf.RobotModelFromURDF(urdf_file=skrobot.data.pr2_urdfpath())
-def initialize():
-    robot_model.translation = np.zeros(3)
-    robot_model.rotation = np.eye(3)
-    robot_model.translate([1.0, 0.0, 0.0])
+rarm_end_coords = skrobot.coordinates.CascadedCoords(
+        parent=robot_model.r_gripper_tool_frame, 
+        name='rarm_end_coords')
 
-initialize()
-robot_model.rotate(pi*0.5, 'z', 'world')
-print(robot_model.worldpos())
-
-initialize()
-robot_model.rotate(pi*0.5, 'z', 'local')
-print(robot_model.worldpos())
+print(rarm_end_coords.worldpos())
+co = Coordinates(pos = [1.0, 1.0, 1.0], rot=rpy_matrix(0.3, 0.3, 0.3))
+robot_model.newcoords(co)
+print(rarm_end_coords.worldpos())
