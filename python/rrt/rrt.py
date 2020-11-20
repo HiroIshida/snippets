3b70bc453c287c475a267760125e6a1d5082185c
HiroIshida <spitfire.docomo@gmail.com>
2020-11-21 03:58:50 +0900

    x -> q

--- python/rrt/rrt.py
+++ python/rrt/rrt.py
@@ -14,62 +14,62 @@ class ConfigurationSpace(object):
         return np.random.rand(self.n_dof) * w + self.b_min
 
 class RapidlyExploringRandomTree(object): 
-    def __init__(self, cspace, x_start, x_goal, sdf, N_maxiter=10000):
+    def __init__(self, cspace, q_start, q_goal, sdf, N_maxiter=10000):
         self.cspace = cspace
         self.eps = 0.1
         self.n_resolution = 10
         self.N_maxiter = N_maxiter
-        self.x_goal = np.array(x_goal)
+        self.q_goal = np.array(q_goal)
         self.sdf = sdf
 
         # reserving memory is the key 
-        self.X_sample = np.zeros((N_maxiter, cspace.n_dof))
+        self.Q_sample = np.zeros((N_maxiter, cspace.n_dof))
         self.idxes_parents = np.zeros(N_maxiter, dtype='int64')
 
         # set initial sample
         self.n_sample = 1
-        self.X_sample[0] = x_start
+        self.Q_sample[0] = q_start
         self.idxes_parents[0] = 0 # self reference
 
     @property
-    def x_start(self):
+    def q_start(self):
         return self.X_sample[0]
 
     def extend(self):
         def unit_vec(vec):
             return vec/np.linalg.norm(vec)
 
-        x_rand = self.cspace.sample()
-        x_rand_copied = np.repeat(x_rand.reshape(1, -1), self.n_sample, axis=0)
+        q_rand = self.cspace.sample()
+        q_rand_copied = np.repeat(q_rand.reshape(1, -1), self.n_sample, axis=0)
 
-        sqdists = np.sum((self.X_sample[:self.n_sample] - x_rand_copied)**2, axis=1)
+        sqdists = np.sum((self.Q_sample[:self.n_sample] - q_rand_copied)**2, axis=1)
         idx_nearest = np.argmin(sqdists)
-        x_nearest = self.X_sample[idx_nearest]
-        if np.linalg.norm(x_rand - x_nearest) > self.eps:
-            x_new = x_nearest + unit_vec(x_rand - x_nearest) * self.eps
+        q_nearest = self.Q_sample[idx_nearest]
+        if np.linalg.norm(q_rand - q_nearest) > self.eps:
+            q_new = q_nearest + unit_vec(q_rand - q_nearest) * self.eps
         else:
-            x_new = x_rand
+            q_new = q_rand
 
         # update tree
-        if sdf(x_new) > 0:
-            self.X_sample[self.n_sample] = x_new
+        if sdf(q_new) > 0:
+            self.Q_sample[self.n_sample] = q_new
             self.idxes_parents[self.n_sample] = idx_nearest
             self.n_sample += 1
 
     def show(self):
         fig, ax = plt.subplots()
         n = self.n_sample
-        ax.scatter(self.X_sample[:n, 0], self.X_sample [:n, 1], c="black")
-        for x, parent_idx in zip(self.X_sample[:n], self.idxes_parents[:n]):
-            x_parent = self.X_sample[parent_idx]
-            ax.plot([x[0], x_parent[0]], [x[1], x_parent[1]], color="red")
+        ax.scatter(self.Q_sample[:n, 0], self.Q_sample [:n, 1], c="black")
+        for q, parent_idx in zip(self.Q_sample[:n], self.idxes_parents[:n]):
+            q_parent = self.Q_sample[parent_idx]
+            ax.plot([q[0], q_parent[0]], [q[1], q_parent[1]], color="red")
 
 
 if __name__=='__main__':
     b_min = np.zeros(2)
     b_max = np.ones(2)
     cspace = ConfigurationSpace(b_min, b_max)
-    sdf = lambda x: np.linalg.norm(x - np.array([0.5, 0.5])) - 0.3
+    sdf = lambda q: np.linalg.norm(q - np.array([0.5, 0.5])) - 0.3
     rrt = RapidlyExploringRandomTree(cspace, [0.1, 0.1], [0.9, 0.9], sdf)
     import time
     ts = time.time()
