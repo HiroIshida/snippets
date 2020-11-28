ef74676239f6d75bc759f72304b925be25742d1d
HiroIshida <spitfire.docomo@gmail.com>
2020-11-28 19:41:36 +0900

    completed swept sphere

--- python/swept_sphere/main.py
+++ python/swept_sphere/main.py
@@ -1,50 +1,106 @@
 import skrobot
 import numpy as np
-from skrobot.models import Box, MeshLink, Axis
+from skrobot.models import Box, MeshLink, Axis, Sphere
 from sklearn.covariance import EmpiricalCovariance
-m = MeshLink("./forearm.obj")
 
-debug = False
-if debug:
-    viewer = skrobot.viewers.TrimeshSceneViewer(resolution=(640, 480))
-    viewer.add(m)
-    viewer.show()
+def debug_view(h_center_min, h_center_max, R, verts_mapped):
+    import matplotlib.pyplot as plt 
+    fig, ax = plt.subplots()
+    ax.axis('equal')
+    create_circle = lambda h : ax.add_artist(plt.Circle((0, h), R, color="red", fill=False, ls="--", lw=2))
+    [ax.add_artist(create_circle(h)) for h in np.linspace(h_center_min, h_center_max, 5)]
+    ax.scatter(verts_mapped[:, 1], verts_mapped[:, 0])
+    plt.show()
 
-vertices_ = m.visual_mesh.vertices  
-mean = np.mean(vertices_, axis=0)
-vertices = np.array([v - mean for v in vertices_])
-#vertices = vertices_ - mean[None, :]
-cov = EmpiricalCovariance().fit(vertices)
-W, V = np.linalg.eig(cov.covariance_)
-pts_converted = vertices.dot(V)
-radius_sqrt = pts_converted[:, 1]**2 + pts_converted[:, 2]**2
-R = np.sqrt(np.max(radius_sqrt)) * 1.0
+def compute_swept_sphere(visual_mesh, 
+        n_sphere=5, 
+        margin_factor=1.01, 
+        with_debug_view=False):
 
-def condition_positive(h):
-    sphere_heights = h + np.sqrt(R**2 - radius_sqrt)
-    return np.all(sphere_heights > pts_converted[:, 0])
+    verts = visual_mesh.vertices
+    mean = np.mean(verts, axis=0)
+    verts_slided = verts - mean[None, :]
+    cov = EmpiricalCovariance().fit(verts_slided)
+    eig_vals, basis_tf_mat = np.linalg.eig(cov.covariance_)
+    verts_mapped = verts_slided.dot(basis_tf_mat)
 
-def condition_negative(h):
-    sphere_heights = h - np.sqrt(R**2 - radius_sqrt)
-    return np.all(sphere_heights < pts_converted[:, 0])
+    def inverse_map(verts): # use this in the end of this function
+        return verts.dot(basis_tf_mat.T) + mean[None, :]
 
-h_vert_max = np.max(pts_converted[:, 0])
-h_vert_min = np.min(pts_converted[:, 0])
-h_max_cands = np.linspace(0.0, h_vert_max, 50)
-h_min_cands = np.linspace(0, h_vert_min, 50)
+    principle_axis = np.argmax(eig_vals)
+    h_vert_max = np.max(verts_mapped[:, principle_axis])
+    h_vert_min = np.min(verts_mapped[:, principle_axis])
 
-idx_p = np.where([condition_positive(h) for h in h_max_cands])[0][0]
-idx_n = np.where([condition_negative(h) for h in h_min_cands])[0][0]
-# max and min height of the swept sphere
-h_max = h_max_cands[idx_p]
-h_min = h_min_cands[idx_n]
+    ## compute radius
+    if principle_axis == 0:
+        plane_axes = [1, 2]
+    elif principle_axis == 1:
+        plane_axes = [2, 0]
+    else:
+        plane_axes = [0, 1]
 
-import matplotlib.pyplot as plt 
-fig, ax = plt.subplots()
-ax.scatter(pts_converted[:, 1], pts_converted[:, 0])
-c1 = plt.Circle((0, h_max), R, color='r', fill=False, ls="--", lw=2)
-c2 = plt.Circle((0, h_min), R, color='r', fill=False, ls="--", lw=2)
-ax.add_artist(c1)
-ax.add_artist(c2)
-ax.axis('equal')
-plt.show()
+    def determine_radius(verts_2d_projected):
+        X, Y = verts_2d_projected.T
+        radius_vec = np.sqrt(X**2 + Y**2)
+        R = np.max(radius_vec)
+        return R
+
+    margin_factor = 1.01
+    R = determine_radius(verts_mapped[:, plane_axes]) * margin_factor
+    sqraidus_vec = np.sum(verts_mapped[:, plane_axes] ** 2, axis=1)
+    h_vec = verts_mapped[:, principle_axis]
+
+    def get_h_center_max():
+        def cond_all_inside_positive(h_center_max):
+            sphere_heights = h_center_max + np.sqrt(R**2 - sqraidus_vec)
+            return np.all(sphere_heights > h_vec)
+        # get first index that satisfies the condition
+        h_cand_list = np.linspace(0, h_vert_max, 30)
+        idx = np.where([cond_all_inside_positive(h) for h in h_cand_list])[0][0]
+        h_center_max = h_cand_list[idx]
+        return h_center_max
+
+    def get_h_center_min():
+        def cond_all_inside_negative(h_center_min):
+            sphere_heights = h_center_min - np.sqrt(R**2 - sqraidus_vec)
+            return np.all(h_vec > sphere_heights)
+        # get first index that satisfies the condition
+        h_cand_list = np.linspace(0, h_vert_min, 30)
+        idx = np.where([cond_all_inside_negative(h) for h in h_cand_list])[0][0]
+        h_center_min = h_cand_list[idx]
+        return h_center_min
+
+    h_center_max = get_h_center_max()
+    h_center_min = get_h_center_min()
+
+    if with_debug_view:
+        debug_view(h_center_min, h_center_max, R, verts_mapped)
+
+    h_centers = np.linspace(h_center_min, h_center_max, n_sphere)
+
+    centers_feature_space = np.zeros((n_sphere, 3)) # reserve
+    centers_feature_space[:, principle_axis] = h_centers
+    centers_original_space = inverse_map(centers_feature_space)
+    return centers_original_space, R
+
+if __name__=='__main__':
+    m = MeshLink("./forearm.obj") # PR2
+    centers, R = compute_swept_sphere(m.visual_mesh, 30, False)
+    verts = m.visual_mesh.vertices
+    # any v in verts must be include at least a sphere
+    logicals_list = [np.sum((verts - c[None, :])**2, axis=1) < R**2 \
+            for c in centers]
+    logicals_array = np.array(logicals_list)
+    res_for_each_vert = np.any(logicals_array, axis=0)
+    res = np.all(res_for_each_vert)
+    assert res, "test fail"
+    print(res)
+
+    # testing
+    if False: # visualize
+        import time
+        viewer = skrobot.viewers.TrimeshSceneViewer(resolution=(640, 480))
+        [viewer.add(s) for s in [Sphere(radius=R, pos=c) for c in centers]]
+        viewer.add(m)
+        viewer.show()
+        time.sleep(3)
