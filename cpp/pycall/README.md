f9f60be61efb08dd1a50ca074825aeb9d4de1d4f
HiroIshida <spitfire.docomo@gmail.com>
2020-12-24 06:52:15 +0900

    finish pycall demo

--- cpp/pycall/README.md
+++ cpp/pycall/README.md
@@ -10,5 +10,24 @@ pyimport(name::String) =
                              (Ptr{Uint8},), bytestring(name)))
 ```
 
+Imported module is stored in `PyObject`. Then, module function can be called by
+```julia
+(o::PyObject)(args...; kwargs...) =
+    return convert(PyAny, _pycall!(PyNULL(), o, args, kwargs))
+```
+
+```julia
+function __pycall!(ret::PyObject, pyargsptr::PyPtr, o::Union{PyObject,PyPtr},
+  kw::Union{Ptr{Cvoid}, PyObject})
+    disable_sigint() do
+        retptr = @pycheckn ccall((@pysym :PyObject_Call), PyPtr, (PyPtr,PyPtr,PyPtr), o,
+                        pyargsptr, kw)
+        pydecref_(ret)
+        setfield!(ret, :o, retptr)
+    end
+    return ret #::PyObject
+end
+```
+
 Notes :
 In python3x, `PySys_SetPath` takes `wchat_t*` type as a parameter. But is python2x, it takes normal `char*`. As for converting `char*` to `wchar_t*` please see : https://stackoverflow.com/questions/8032080/how-to-convert-char-to-wchar-t
--- cpp/pycall/test.cpp
+++ cpp/pycall/test.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <Python.h>
 #include <string>
+#include <iostream>
 
 using namespace std;
 
@@ -10,8 +11,27 @@ int main()
   Py_Initialize();
   string site_package_path = "/home/hiro/.local/lib/python2.7/site-packages";
   PySys_SetPath("/home/hiro/.local/lib/python2.7/site-packages");
-  auto module = PyImport_ImportModule("tinyfk");
-  PyRun_SimpleString("print('Hello World from Embedded Python!!!')");
+  PyObject* module = PyImport_ImportModule("tinyfk");
+  char* name = PyModule_GetName(module);
+  std::cout << "imported : " << name << std::endl; 
+  char* filepath = PyModule_GetFilename(module);
+  std::cout << filepath << std::endl; 
+
+  string urdf_file = "/home/hiro/.skrobot/pr2_description/pr2.urdf";
+  PyObject* robot_model = PyObject_CallMethod(module, const_cast<char*>("RobotModel"), 
+      const_cast<char*>("s"), urdf_file.c_str());
+
+  // https://docs.python.org/ja/3/c-api/call.html#c.PyObject_CallMethod
+  string joint_names = "r_elbow_flex_joint";
+  // for format https://docs.python.org/3/c-api/arg.html
+  // to set list of [string] 
+  PyObject* indices = PyObject_CallMethod(robot_model, const_cast<char*>("get_joint_ids"), const_cast<char*>("[s]"), joint_names.c_str());
+
+  PyObject* idx_pyo = PyList_GetItem(indices, 0);
+  int idx = PyLong_AsLong(idx_pyo);
+  std::cout << "corresponding link index is : " << idx << std::endl; 
+
+  PyRun_SimpleString("print('tinyfk demo')");
   Py_Finalize();
   return 0;
 }
