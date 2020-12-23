374ce94975d0135add1c9d165da0f6c90dc49489
HiroIshida <spitfire.docomo@gmail.com>
2020-12-24 05:40:55 +0900

    calling tinyfk using python2.7

--- cpp/pycall/README.md
+++ cpp/pycall/README.md
@@ -9,3 +9,6 @@ pyimport(name::String) =
     PyObject(@pycheckn ccall(pyfunc(:PyImport_ImportModule), PyPtr,
                              (Ptr{Uint8},), bytestring(name)))
 ```
+
+Notes :
+In python3x, `PySys_SetPath` takes `wchat_t*` type as a parameter. But is python2x, it takes normal `char*`. As for converting `char*` to `wchar_t*` please see : https://stackoverflow.com/questions/8032080/how-to-convert-char-to-wchar-t
--- cpp/pycall/test.cpp
+++ cpp/pycall/test.cpp
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <Python.h>
+#include <string>
+
+using namespace std;
 
 int main()
 {
-	PyObject* pInt;
-	Py_Initialize();
-	PyRun_SimpleString("print('Hello World from Embedded Python!!!')");
-	Py_Finalize();
-	return 0;
+  PyObject* pInt;
+  Py_Initialize();
+  string site_package_path = "/home/hiro/.local/lib/python2.7/site-packages";
+  PySys_SetPath("/home/hiro/.local/lib/python2.7/site-packages");
+  auto module = PyImport_ImportModule("tinyfk");
+  PyRun_SimpleString("print('Hello World from Embedded Python!!!')");
+  Py_Finalize();
+  return 0;
 }
