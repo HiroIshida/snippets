ea163ff75f4669ea71d0085397e713d0aefbb49c
HiroIshida <spitfire.docomo@gmail.com>
2020-12-26 00:40:18 +0900

    add some utils

--- cpp/eigen/eigen_block_util.cpp
+++ cpp/eigen/eigen_block_util.cpp
@@ -6,7 +6,7 @@
 using namespace Eigen;
 using namespace std;
 
-struct SubMatrix
+struct SubMatrix // coll major (same as eigen)
 {
   double* _data; // beginning of the block matrix
   int _i_begin;
@@ -24,6 +24,16 @@ struct SubMatrix
     _n_block(n), _m_block(m),
     _n_whole(mat.rows()), _m_whole(mat.cols()) {}
 
+  SubMatrix(Eigen::MatrixXd& mat) :
+    _data(mat.data()),
+    _i_begin(0), _j_begin(0),
+    _n_block(mat.rows()), _m_block(mat.cols()),
+    _n_whole(_n_block), _m_whole(_m_block) {}
+
+  SubMatrix(double* data, int i_begin, int j_begin, int n, int m, int n_whole, int m_whole) :
+    _data(data), _i_begin(i_begin), _j_begin(j_begin), _n_block(n), _m_block(m), 
+    _n_whole(n_whole), _m_whole(m_whole) {}
+
   inline int get_idx(int i, int j){
     assert(i<_n_whole && "out of index");
     assert(j<_m_whole && "out of index");
@@ -35,20 +45,42 @@ struct SubMatrix
   double& get(int i, int j){
     return _data[this->get_idx(i, j)];
   }
+
+  SubMatrix block(int i, int j, int n, int m){
+    int i_begin_new = _i_begin + i;
+    int j_begin_new = _j_begin + j;
+    SubMatrix mat = {_data, i_begin_new, j_begin_new, _n_block, _m_block, _n_whole, _m_whole};
+    return mat;
+  }
+
+  SubMatrix slice(int i){ // we consider matrix is coll major. 
+    return this->block(0, i, _n_block, 1);
+  }
+
   double& operator() (int i, int j){
     return _data[this->get_idx(i, j)];
+  }
+
+  double& operator[] (int i){ // access to sliced matrix
+    return _data[this->get_idx(i, 0)];
   }
 };
 
 int main(){
   {
-    MatrixXd m(8, 6);
-    auto bm = SubMatrix(m, 2, 2, 2, 2);
-    bm(0, 0) = 1;
-    bm(0, 1) = 2;
-    bm(1, 0) = 3;
-    bm(1, 1) = 4;
-    std::cout << m << std::endl; 
+    MatrixXd m_(8, 6);
+    SubMatrix m(m_); 
+    auto sm = m.block(1, 1, 2, 2);
+    sm(0, 0) = 1;
+    sm(0, 1) = 2;
+    sm(1, 0) = 3;
+    sm(1, 1) = 4;
+
+    SubMatrix vec = m.slice(4);
+    for(int i=0; i<8; i++){
+      vec[i] = 1;
+    }
+    std::cout << m_ << std::endl; 
   }
 
   // check how does it slow
