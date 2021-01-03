953ec6d08e67bab32ea7390f3ac902e4b78fb2fa
HiroIshida <spitfire.docomo@gmail.com>
2021-01-03 18:31:49 +0900

    add bench

---
+++ julia/bench/index_access.jl
@@ -0,0 +1,33 @@
+struct Joint
+    id::Int
+end
+
+function main()
+    joints = Joint[]
+    idxes = Int[]
+    for i in 1:100
+        push!(joints, Joint(i))
+        push!(idxes, i)
+    end
+
+    function bench1()
+        for i in 1:10000
+            for j in 1:100
+                joint = joints[j]
+            end
+        end
+    end
+    @time bench1()
+
+    function bench2()
+        for i in 1:10000
+            for j in 1:100
+                idx = idxes[j]
+                joint = joints[idx]
+            end
+        end
+    end
+    @time bench2()
+end
+
+main()
