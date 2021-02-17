f9ed345af1ef0d6b4695a1b90a719daae3d874a9
HiroIshida <spitfire.docomo@gmail.com>
2021-02-18 00:26:12 +0900

    classification in S1xR example

--- python/svm_experiment/cylinder.py
+++ python/svm_experiment/cylinder.py
@@ -1,3 +1,4 @@
+from sklearn import svm
 from sklearn.metrics import pairwise
 import matplotlib.pyplot as plt 
 import numpy as np
@@ -29,5 +30,55 @@ def plot_cylinder_coordinate(center):
     cs = ax.contourf(lin_circle, lin_euclidean, dist_grid, cmap = 'jet', zorder=1)
     plt.show()
 
-plot_cylinder_coordinate([1.3, 0])
+def generate_cylinder_kernel(gammas = None):
+    # as for closure in py2x see:
+    # https://stackoverflow.com/questions/3190706/nonlocal-keyword-in-python-2-x
+    d = {'gammas': gammas}
+    def kern(X_, Y_ = None):
+        gammas = d['gammas']
+        n_x, m_x = X_.shape
+        assert m_x == 2
+
+        if gammas is None:
+            gammas = np.ones(m_x)
+        if Y_ is None:
+            Y_ = X_
+        n_y = Y_.shape[0]
+
+        mat = np.zeros((n_x, n_y))
+        for j in range(n_y):
+            Y_repeated = np.repeat(np.array([Y_[j]]), n_x, axis=0)
+            dists = cylinder_metric(X_, Y_repeated)
+            mat[:, j] = np.exp(- 0.01 * dists**2)
+        return mat
+    return kern
+
+#plot_cylinder_coordinate([0.5, 0.5])
+
+use_svm = True
+if use_svm:
+    dataset1 = [
+            [[0, 0], [0, 0], [1, 1]],
+            [0, 0, 1]
+            ]
+    dataset2 = [
+            [[0, 0], [1., 1]],
+            [0, 1]
+            ]
+    kern = generate_cylinder_kernel()
+    clf = svm.SVC(kernel = kern)
+    myfit = lambda dataset : clf.fit(dataset[0], dataset[1])
+    myfit(dataset2)
+
+    N = 100
+    lin_circle = np.linspace(-math.pi, math.pi, N)
+    lin_euclidean = np.linspace(-4.0, 4.0, N)
+    mesh_grid = np.meshgrid(lin_circle, lin_euclidean)
+    pts = np.array(zip(mesh_grid[0].flatten(), mesh_grid[1].flatten()))
+    preds_ = clf.decision_function(pts)
+    preds = preds_.reshape(N, N)
+
+    fig, ax = plt.subplots()
+    cs = ax.contourf(lin_circle, lin_euclidean, preds, cmap = 'jet')
+    plt.show()
 
