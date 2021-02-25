fb39682b470f998d043abaf0dd27f0055b5cabed
HiroIshida <spitfire.docomo@gmail.com>
2021-02-26 00:26:34 +0900

    udpated dmp

--- python/dmppy/dmp.py
+++ python/dmppy/dmp.py
@@ -1,118 +1,124 @@
 import numpy as np
-
-class DMP(object):
-    def __init__(self, w=None, g=None):
-        # Transformation system
-        self.alpha = 25.0             # = D = 20.0
-        self.beta = self.alpha / 4.0  # = K / D = 100.0 / 20.0 = 5.0
-        # Canonical system
-        self.alpha_t = 2
-        self.w = w
-        self.g = g
-
-    @classmethod
-    def imitate(cls, X, tau, n_features):
-        dmp = DMP()
-        dmp.g = X[-1, :]
-        dmp.w = dmp._imitate(X, tau, n_features)
-        return dmp
-
-    def phase(self, n_steps):
-        phases = np.exp(-self.alpha_t * np.linspace(0, 1, n_steps))
-        return phases
-
-    def spring_damper(self, x0, g, tau, s, X, Xd):
-        return self.alpha * (self.beta * (g - X) - tau * Xd) / tau ** 2
-
-    def forcing_term(self, x0, g, tau, w, s, X):
-        n_features = w.shape[1]
-        f = np.dot(w, self._features(tau, n_features, s))
-        return f
-
-    def _features(self, tau, n_features, s):
-        c = self.phase(n_features)
-        h = np.diff(c)
-        h = np.hstack((h, [h[-1]]))
-        phi = np.exp(-h * (s - c) ** 2)
-        return s * phi / phi.sum()
-
-    def _imitate(self, X, tau, n_features):
-        n_steps, n_dims = X.shape
-        dt = tau / float(n_steps - 1)
-        g = X[-1, :]
-
-        Xd = np.vstack((np.zeros((1, n_dims)), np.diff(X, axis=0) / dt))
-        Xdd = np.vstack((np.zeros((1, n_dims)), np.diff(Xd, axis=0) / dt))
-
-        g_repeated = np.repeat(g[np.newaxis, :], n_steps, axis=0)
-        F = tau * tau * Xdd - self.alpha * (self.beta * (g_repeated - X)
-                                            - tau * Xd)
-
-        design = np.array([self._features(tau, n_features, s)
-                           for s in self.phase(n_steps)])
-        #w = np.linalg.lstsq(design, F)[0].T
-        from sklearn.linear_model import Ridge
-        lr = Ridge(alpha=1.01, fit_intercept=False)
-        lr.fit(design, F)
-        w = lr.coef_
-        print(w)
-
-        return w
-
-    def trajectory(self, x0, tau, dt):
-        x = x0.copy()
-        xd = np.zeros_like(x, dtype=np.float64)
-        xdd = np.zeros_like(x, dtype=np.float64)
-        X = [x0.copy()]
-        Xd = [xd.copy()]
-        Xdd = [xdd.copy()]
-
-        # Internally, we do Euler integration usually with a much smaller step size
-        # than the step size required by the system
-        internal_dt = dt
-        n_internal_steps = int(tau / internal_dt)
-        steps_between_measurement = int(dt / internal_dt)
-        t_seq = [0]
-                
-
-        # Usually we would initialize t with 0, but that results in floating point
-        # errors for very small step sizes. To ensure that the condition t < tau
-        # really works as expected, we add a constant that is smaller than
-        # internal_dt.
-        t = 0.5 * internal_dt
-        ti = 0
-        S = self.phase(n_internal_steps + 1)
-        while t < tau:
-            t += internal_dt
-            t_seq.append(t)
-            ti += 1
-            s = S[ti]
-
-            x += internal_dt * xd
-            xd += internal_dt * xdd
-
-            sd = self.spring_damper(x0, self.g, tau, s, x, xd)
-            f = self.forcing_term(x0, self.g, tau, self.w, s, x) 
-            xdd = sd + f 
-
-            X.append(x.copy())
-            Xd.append(xd.copy())
-            Xdd.append(xdd.copy())
-
-        return np.array(X), np.array(t_seq)
-
-
-print("This implementation doesn't work (under construction)")
-t_end = 2.0
-t_seq = np.linspace(0, t_end, 100)
-x_seq = np.vstack((np.cos(t_seq), np.sin(t_seq))).T
-tau = 1.0
-dmp = DMP.imitate(x_seq, t_end, 1000)
-X, T = dmp.trajectory(np.zeros(2), t_end, 1e-2)
-
+#from sklearn.linear_model import Ridge
 import matplotlib.pyplot as plt
-plt.plot(T, X[:, 0], "ro")
-plt.plot(t_seq, x_seq[:, 0], "bo")
-plt.show()
+import math
+from utils import midpoints
+from utils import differentiate_seq
+
+class MassSpringDamperSystem(object):
+    def __init__(self, alpha=25.0, beta=5.0, center=0.0):
+        self.alpha = alpha
+        self.beta = beta
+        self.center = center
+
+    def acceleration(self, pos, vel):
+        return self.alpha * (self.beta * (self.center - pos) - vel)
+
+class CanonicalSystem(object):
+    def __init__(self, alpha=1.0):
+        self.alpha = alpha
+
+    def phase(self, t):
+        return np.exp(-self.alpha * t)
+
+    def basis_centers(self, n_basis):
+        """
+        return center of basis function (in phase space, not in time space) 
+        """
+        t_almost_converge = 5.0/self.alpha
+        print(t_almost_converge)
+
+        t_seq_aug = np.linspace(0, t_almost_converge, n_basis + 1)
+        t_seq = midpoints(t_seq_aug)
+        phase_seq = np.array([self.phase(t) for t in t_seq])
+        return phase_seq
+
+class DynamicalMovementPrimitive(object):
+
+    def __init__(self, n_feature=100):
+        self.system = MassSpringDamperSystem()
+        self.canonical = CanonicalSystem()
+        self.basis_centers = self.canonical.basis_centers(n_feature)
+
+        tmp = np.diff(self.basis_centers)
+        self.rbf_length = np.hstack([tmp, tmp[-1]])
+
+        self.n_feature = n_feature
+
+        self.weights = None
+        self.pos_start = None
+        self.pos_goal = None
+
+    def _rbf_vals(self, phase):
+        rbf = lambda diff: np.exp(- 1.0/(1.0 * self.rbf_length**2) * diff**2)
+        diffs = phase - self.basis_centers
+        return rbf(diffs)
+
+    def _forcing_term(self, phase):
+        rbf_vals = self._rbf_vals(phase)
+        left_term = rbf_vals.dot(self.weights)/np.sum(self.weights)
+        return left_term * phase * (self.pos_goal - self.pos_start)
+
+    def fit(self, pos_seq, t_end):
+        """
+        pos_seq: trajectory numpy.array 1dim with equality separated time steps
+        n_feature : int
+        """
+
+        n_step_ = len(pos_seq)
+        dt = t_end / (n_step_ - 1)
+        t_seq_pos = np.linspace(0, t_end, n_step_)
+        t_seq_vel = midpoints(t_seq)
+        t_seq_acc = midpoints(t_seq_vel)
+
+        vel_seq = differentiate_seq(pos_seq, dt)
+        acc_seq = differentiate_seq(vel_seq, dt)
+
+        # adjust size of pos_seq and vel_seq to the same as acc_seq
+        pos_seq = midpoints(midpoints(pos_seq))
+        vel_seq = midpoints(vel_seq)
+
+        system_acc_seq = [self.system.acceleration(pos, vel) for pos, vel in zip(pos_seq, vel_seq)]
+
+        force_desired = acc_seq - system_acc_seq
+
+        # create least square problem : minimize_w ||Aw = b||^2
+        pos_start = pos_seq[0]
+        pos_goal = pos_seq[-1]
+
+        n_step = len(force_desired)
+        phase_seq = [self.canonical.phase(t) for t in t_seq_acc]
+
+        A = np.zeros((n_step, self.n_feature))
+        for i in range(n_step):
+            phase = phase_seq[i]
+            xgy = phase * (pos_goal - pos_start) # term of x * (g - y0) in the paper
+            rbf_vals = self._rbf_vals(phase)
+            A[i, :] = rbf_vals/np.sum(rbf_vals) * xgy
+        b = force_desired
+
+        w = np.linalg.inv(A.T.dot(A) + 10.0 * np.eye(self.n_feature)).dot(A.T).dot(b)
+        self.weights = w
+        self.pos_start = pos_start
+        self.pos_goal = pos_goal
+
+        print(A.dot(w))
+        print(b)
+
+
+
+if __name__=='__main__':
+    t_end = 3.14
+    t_seq = np.linspace(0, t_end, 100)
+    pos_seq = np.cos(t_seq)
+    dmp = DynamicalMovementPrimitive(n_feature=200)
+    dmp.fit(pos_seq, t_end)
+
+    phase_lin = [dmp.canonical.phase(t) for t in t_seq]
+    force_seq = [dmp._forcing_term(x) for x in phase_lin]
+    plt.plot(t_seq, force_seq)
+    plt.show()
+
+
 
 
