74d9b13c087aeb0beb408693060d457fc63b6021
HiroIshida <spitfire.docomo@gmail.com>
2021-04-13 01:33:23 +0900

    tweak

--- julia/svm_contourf.jl
+++ julia/svm_contourf.jl
@@ -2,10 +2,10 @@ import LIBSVM
 import PyPlot
 
 function simple_data()
-    x1 = [0, 0.5, 0,   -0.2, -0.3, 0.8, 0.8, 0.5, 0.4]
-    x2 = [0, 0.1, 0.9, -0.9, 0.1, -0.2, 0.2, 0.5, -0.3]
+    x1 = [0, 0.2, 0.5, 0,   -0.2, -0.3, 0.8, 0.8, 0.5, 0.4]
+    x2 = [0, 0.9, 0.1, 0.9, -0.9, 0.1, -0.2, 0.2, 0.5, -0.3]
     X = transpose(hcat(x1, x2))
-    y = [true, true, false, false, false, false, false, false, false]
+    y = [true, true, true, false, false, false, false, false, false, false]
     return X, y
 end
 
@@ -16,5 +16,5 @@ xlin = range(0, 1, length=100)
 ylin = range(0, 1, length=100)
 f(x) = LIBSVM.svmpredict(model, reshape(x, 2, 1))[2][1]
 fs = [f([x y]) for x in xlin, y in ylin]
-PyPlot.contourf(xlin, ylin, fs)
+PyPlot.contourf(xlin, ylin, fs')
 PyPlot.plt.colorbar()
