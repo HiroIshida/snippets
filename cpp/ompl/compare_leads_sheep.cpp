5238f5b41ae16392a1330d9ca3f8a5bc369a9274
HiroIshida <spitfire.docomo@gmail.com>
2021-04-15 11:23:26 +0900

    heuristics seems to work fine

--- cpp/ompl/compare_leads_sheep.cpp
+++ cpp/ompl/compare_leads_sheep.cpp
@@ -2,21 +2,19 @@
 #include <ompl/base/spaces/SE2StateSpace.h>
 #include <ompl/base/ScopedState.h>
 #include <ompl/base/State.h>
-
 #include <array>
 #include <iostream>
 
-using namespace std;
 using A3 = std::array<double, 3>;
 
-void set_state(A3 pose, ompl::base::ScopedState<ompl::base::SE2StateSpace>& s)
+void set_state(const A3& pose, ompl::base::ScopedState<ompl::base::SE2StateSpace>& s)
 {
   s->setX(pose[0]);
   s->setY(pose[1]);
   s->setYaw(pose[2]);
 }
 
-double reeds_shepp_cost(A3 arr_start, A3 arr_goal){
+double reeds_shepp_cost(const A3& arr_start, const A3& arr_goal){
   ompl::base::StateSpacePtr rspace(new ompl::base::ReedsSheppStateSpace());
   ompl::base::StateSpacePtr space(new ompl::base::SE2StateSpace());
   ompl::base::ScopedState<ompl::base::SE2StateSpace> start(space), goal(space);
@@ -27,6 +25,8 @@ double reeds_shepp_cost(A3 arr_start, A3 arr_goal){
 
 int main(){
   A3 a = {0, 0, 0};
-  A3 b = {1, 1, 1};
-  std::cout << reeds_shepp_cost(a, b) << std::endl; 
+  A3 b = {1, 0, 0};
+  A3 c = {0, 1, 0};
+  std::cout << "cost to move along x-axis: " << reeds_shepp_cost(a, b) << std::endl; 
+  std::cout << "cost to move along y-axis: " << reeds_shepp_cost(a, c) << std::endl; 
 }
