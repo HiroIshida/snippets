83ceab5756d4dc50a3465df6913caf3a84ca430c
HiroIshida <spitfire.docomo@gmail.com>
2021-04-15 15:53:49 +0900

    tweak

--- ros/cpp_msg_dump/main.cpp
+++ ros/cpp_msg_dump/main.cpp
@@ -1,5 +1,7 @@
+#include <fstream>
 #include <ros/ros.h>
 #include <nav_msgs/OccupancyGrid.h>
+namespace ser = ros::serialization;
 
 nav_msgs::OccupancyGrid::ConstPtr occupancy_grid_;
 
@@ -7,13 +9,18 @@ void onOccupancyGrid(const nav_msgs::OccupancyGrid::ConstPtr & msg)
 {
   ROS_INFO("occupancy grid recieved");
   occupancy_grid_ = msg;
+  uint32_t serial_size = ros::serialization::serializationLength(*msg);
+  boost::shared_array<uint8_t> buffer(new uint8_t[serial_size]);
+  ser::OStream stream(buffer.get(), serial_size);
+  ser::serialize(stream, *msg);
+  //std::ofstream stream("tmp.out");
 }
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "dumper");
   ros::NodeHandle nh;
 
-  ros::Subscriber sub = nh.subscribe("input/occupancy_grid", 1, onOccupancyGrid);
+  ros::Subscriber sub = nh.subscribe("/planning/scenario_planning/parking/costmap_generator/occupancy_grid", 1, onOccupancyGrid);
   nav_msgs::OccupancyGrid grid;
   ros::spin();
   return 0;
