b8197fd88912378ce81bfd377eb2aebc2c226db9
HiroIshida <spitfire.docomo@gmail.com>
2021-04-18 20:54:11 +0900

    simplify

--- python/spline.py
+++ python/spline.py
@@ -1,97 +1,75 @@
 import numpy as np
 
-class DesignMatrix(object):
-
-    def __init__(self, n_piece):
-        self.A = np.zeros((4 * n_piece, 4 * n_piece))
-        self.b = np.zeros(4 * n_piece)
-        self.idx = 0
-
-    def set_Aval(self, j, vec):
-        # i-th constraint, j-th piece
-        self.A[self.idx, 4*j:4*(j+1)] = vec
-
-    def set_bval(self, val):
-        self.b[self.idx] = val
-
-    def next(self):
-        self.idx += 1
-
 if __name__=='__main__':
     def create_interp(x, y):
         n_piece = len(x) - 1
         n_coef = n_piece * 4
-        M = DesignMatrix(n_piece)
+
+        # try to determine w by solving linear equation A*w = b
+        A = np.zeros((n_coef, n_coef))
+        b = np.zeros(n_coef)
 
         zeros = np.zeros(len(x))
         ones = np.ones(len(x))
         xx = x**2
         xxx = x**3
+
         phi = np.vstack([ones, x, xx, xxx]).T
         phi_d = np.vstack([zeros, ones, 2 * x, 3 * xx]).T
         phi_dd = np.vstack([zeros, zeros, 2 * ones, 6 * x]).T
 
+        idx = 0
+
         # 0-th order condition 
         for i in range(n_piece):
-            M.set_Aval(i, phi[i, :])
-            M.set_bval(y[i])
-            M.next()
+            # left
+            A[idx, 4*i:4*(i+1)] = phi[i, :]
+            b[idx] = y[i]
+            idx += 1
 
-            M.set_Aval(i, phi[i+1, :])
-            M.set_bval(y[i+1])
-            M.next()
+            # right
+            A[idx, 4*i:4*(i+1)] = phi[i+1, :]
+            b[idx] = y[i+1]
+            idx += 1
 
+        # 1-st order condition
         for i in range(n_piece-1):
-            M.set_Aval(i, phi_d[i+1, :])
-            M.set_Aval(i+1, -phi_d[i+1, :])
-            M.set_bval(0)
-            M.next()
+            A[idx, 4*i:4*(i+1)] = phi_d[i+1, :]
+            A[idx, 4*(i+1):4*(i+2)] = -phi_d[i+1, :]
+            idx += 1
 
+        # 2-nd order condition
         for i in range(n_piece-1):
-            M.set_Aval(i, phi_dd[i+1, :])
-            M.set_Aval(i+1, -phi_dd[i+1, :])
-            M.set_bval(0)
-            M.next()
+            A[idx, 4*i:4*(i+1)] = phi_dd[i+1, :]
+            A[idx, 4*(i+1):4*(i+2)] = -phi_dd[i+1, :]
+            idx += 1
 
         # natural spline condition for start
-        M.set_Aval(0, phi_dd[0, :])
-        M.set_bval(0)
-        M.next()
+        A[idx, 0:4] = phi_dd[0, :]
+        idx += 1
 
         # natural spline condition for end
-        M.set_Aval(n_piece-1, phi_dd[n_piece, :])
-        M.set_bval(0)
-        M.next()
+        A[idx, -4:] = phi_dd[n_piece, :]
+        idx += 1
 
-        w = np.linalg.inv(M.A).dot(M.b)
+        w = np.linalg.solve(A, b)
 
         def interp(a):
-            idx = min(np.where(a < x))[0] - 1
-            return np.dot(w[idx * 4: (idx+1) * 4], np.array([1, a, a**2, a**3]))
+            i = min(np.where(a < x))[0] - 1
+            return np.dot(w[i * 4: (i+1) * 4], np.array([1, a, a**2, a**3]))
         return interp
 
-    x = np.array([0, 1, 2, 3, 4])
-    y = np.array([1, 2, 3, 2, 1.5])
+    x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    y = np.array([1, 2, 3, 2, 1.5, 1.3, 1.1, 1.0, 0.9, 0.6])
     itp = create_interp(x, y)
+    y = itp(2.0)
 
-    xs = np.linspace(0.1, 3.9, 100)
+    xs = np.linspace(0.1, 8.9, 100)
     ys = [itp(x) for x in xs]
 
     import matplotlib.pyplot as plt
     plt.plot(xs, ys)
     plt.show()
-
-
-
-
-        
-
-
-
-
-
-
-
 
 
 
