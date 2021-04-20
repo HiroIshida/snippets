fc53c9ed3cc0fe8ccef59a13236742989b0e4893
HiroIshida <spitfire.docomo@gmail.com>
2021-04-21 02:41:40 +0900

    Completed demo

--- ros/online_sdf_demo/main.py
+++ ros/online_sdf_demo/main.py
@@ -11,10 +11,11 @@ from skrobot.planner.utils import set_robot_config
 from skrobot.planner import sqp_plan_trajectory
 from skrobot.planner import SweptSphereSdfCollisionChecker
 from skrobot.model.primitives import Box
-from utils import *
+import trimesh
 
+nodebug = True
 with_real_robot = True
-with_move_base = False
+with_move_base = True
 
 robot_model = skrobot.models.PR2()
 
@@ -33,13 +34,27 @@ coll_link_list = [
 tjoint_angle = robot_model.torso_lift_joint.joint_angle()
 robot_model.reset_manip_pose()
 robot_model.torso_lift_joint.joint_angle(tjoint_angle)
-robot_model.head_tilt_joint.joint_angle(0.9)
 
 
-if with_real_robot:
+if nodebug:
     ri = skrobot.interfaces.ros.PR2ROSRobotInterface(robot_model)
+    robot_model.head_tilt_joint.joint_angle(0.9)
     ri.angle_vector(robot_model.angle_vector(), time=1.0, time_scale=1.0)
     ri.wait_interpolation()
+
+    robot_model.head_pan_joint.joint_angle(0.5)
+    ri.angle_vector(robot_model.angle_vector(), time=1.0, time_scale=1.0)
+    ri.wait_interpolation()
+
+    robot_model.head_pan_joint.joint_angle(-0.5)
+    ri.angle_vector(robot_model.angle_vector(), time=1.0, time_scale=1.0)
+    ri.wait_interpolation()
+
+    robot_model.head_pan_joint.joint_angle(0.0)
+    ri.angle_vector(robot_model.angle_vector(), time=1.0, time_scale=1.0)
+    ri.wait_interpolation()
+
+
     if with_move_base:
         # TODO 
         print("NOTE that, to use this, one needs to remove point cloud on robot")
@@ -51,7 +66,7 @@ if with_real_robot:
         print("rec")
         esdf.update(msg)
     rospy.Subscriber(topic_name, Layer, callback)
-    time.sleep(6.0)
+    time.sleep(18.0)
 av_start = get_robot_config(robot_model, joint_list, with_base=False)
 
 # solve inverse kinematics to obtain av_goal
@@ -68,7 +83,7 @@ robot_model.inverse_kinematics(
     move_target=robot_model.rarm_end_coords, rotation_axis=False)
 av_goal = get_robot_config(robot_model, joint_list, with_base=False)
 
-if with_real_robot:
+if nodebug:
     def sdf(X):
         dists = esdf.get_distance(X)
         return dists
@@ -80,24 +95,33 @@ else:
 for link in coll_link_list:
     sscc.add_collision_link(link)
 
+
 # motion planning
 ts = time.time()
-n_waypoint = 10
+n_waypoint = 15
 av_seq = sqp_plan_trajectory(
     sscc, av_start, av_goal, joint_list, n_waypoint,
-    safety_margin=10e-2, with_base=False)
+    safety_margin=0.12, with_base=False)
 print("solving time : {0} sec".format(time.time() - ts))
 
-if with_real_robot:
-    for av in av_seq:
-        set_robot_config(robot_model, joint_list, av, with_base=False)
+b_min = [0.0, -1.0, 0.0]
+b_max = [1.5, 1.0, 1.5]
+pts_inside = esdf.debug_points(b_min, b_max)
+
+viewer = skrobot.viewers.TrimeshSceneViewer(resolution=(640, 480))
+viewer.add(robot_model)
+sscc.add_coll_spheres_to_viewer(viewer)
+tpc = trimesh.PointCloud(pts_inside)
+pclink = skrobot.model.primitives.PointCloudLink(tpc)
+viewer.add(pclink)
+viewer.show()
+
+for av in av_seq:
+    set_robot_config(robot_model, joint_list, av, with_base=False)
+    sscc.update_color()
+    viewer.redraw()
+    if with_real_robot:
         ri.angle_vector(robot_model.angle_vector(), time=0.5, time_scale=1.0)
         ri.wait_interpolation()
-else:
-    viewer = skrobot.viewers.TrimeshSceneViewer(resolution=(640, 480))
-    viewer.add(robot_model)
-    viewer.show()
-    for av in av_seq:
-        set_robot_config(robot_model, joint_list, av, with_base=False)
-        viewer.redraw()
+    else:
         time.sleep(2.0)
