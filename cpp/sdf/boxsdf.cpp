24f02a2c3b4919c725a4c08fe96784c677420a04
HiroIshida <spitfire.docomo@gmail.com>
2021-04-21 17:40:32 +0900

    boxsdf example

---
+++ cpp/sdf/boxsdf.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<Eigen/Core>
+using namespace Eigen;
+
+/* fetched from https://www.iquilezles.org/www/articles/distfunctions/distfunctions.htm
+float sdBox( vec3 p, vec3 b )
+{
+  vec3 q = abs(p) - b;
+  return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0);
+}
+*/
+
+double boxsdf(const Eigen::Vector2d& pos, const Eigen::Vector2d& b)
+{
+  Vector2d q = pos.cwiseAbs() - b;
+  Vector2d left_max(std::max(q(0), 0.0), std::max(q(1), 0.0));
+  double left = left_max.norm();
+  double right = std::min(std::max(q(0), q(1)), 0.0);
+  return left + right;
+}
+int main(){
+  // testing
+  {
+    Vector2d p(1.5, 0);
+    Vector2d b(1, 2);
+    std::cout << (boxsdf(p, b) == 0.5) << std::endl; 
+  }
+  {
+    Vector2d p(0, 0);
+    Vector2d b(1, 2);
+    std::cout << (boxsdf(p, b) == -1.0) << std::endl; 
+  }
+  {
+    Vector2d p(0, 1.5);
+    Vector2d b(1, 2);
+    std::cout << (boxsdf(p, b) == -0.5) << std::endl; 
+  }
+  {
+    Vector2d p(0, 2.5);
+    Vector2d b(1, 2);
+    std::cout << (boxsdf(p, b) == +0.5) << std::endl; 
+  }
+
+  {
+    int N = 1000000;
+    Vector2d p(0, 2.5);
+    Vector2d b(1, 2);
+    clock_t start = clock();
+    for(int i=0; i<N; i++){
+      boxsdf(p, b);
+    }
+    clock_t end = clock();
+    std::cout << (end - start) << " [micro sec]" << std::endl; 
+  }
+
+  //benchmarking
+}
+
