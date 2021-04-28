9b07bbb68a95722b0e1996a359fec5118218d6d9
HiroIshida <spitfire.docomo@gmail.com>
2021-04-28 13:11:54 +0900

    alloc perf

--- cpp/ompl/compare_leads_sheep.cpp
+++ cpp/ompl/compare_leads_sheep.cpp
@@ -23,10 +23,22 @@ double reeds_shepp_cost(const A3& arr_start, const A3& arr_goal){
   return rspace->distance(start.get(), goal.get());
 }
 
+void alloc_perf(int N){
+  for(int i=0; i<N; i++){
+    ompl::base::StateSpacePtr rspace(new ompl::base::ReedsSheppStateSpace());
+    ompl::base::StateSpacePtr space(new ompl::base::SE2StateSpace());
+  }
+}
+
 int main(){
   A3 a = {0, 0, 0};
   A3 b = {1, 0, 0};
   A3 c = {0, 1, 0};
   std::cout << "cost to move along x-axis: " << reeds_shepp_cost(a, b) << std::endl; 
   std::cout << "cost to move along y-axis: " << reeds_shepp_cost(a, c) << std::endl; 
+
+  clock_t start = clock();
+  alloc_perf(20000);
+  clock_t end = clock();
+  std::cout << (end - start)/1000.0 << std::endl;  // 57 [ms]
 }
