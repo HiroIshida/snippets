8ab50cc516ddf2dd96f92fc00f70964787588b7b
HiroIshida <spitfire.docomo@gmail.com>
2021-05-03 01:01:44 +0900

    pydict

--- julia/pycall/pycall_scipy.jl
+++ julia/pycall/pycall_scipy.jl
@@ -7,6 +7,20 @@ function f(x::Vector)
     return norm(x)^2
 end
 
+function df(x::Vector)
+    return 2 * x
+end
+
+function h(x::Vector)
+    return x[1] - 1
+end
+
+function dh(x::Vector)
+    return [1, 0]
+end
+
+eq_dict = Dict("type"=> "eq", "fun"=> h, "jac"=> dh)
+constraints = [eq_dict]
 x0 = [0.5, 0]
-__optimize__.minimize(f, x0, method="SLSQP")
+__optimize__.minimize(f, x0, method="SLSQP", jac=df, constraints=constraints)
 
