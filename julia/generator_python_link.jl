5423f7592196097ab3ab0706b4b4e579289a0ee1
HiroIshida <spitfire.docomo@gmail.com>
2021-05-19 23:55:37 +0900

    Fix

--- julia/generator_python_link.jl
+++ julia/generator_python_link.jl
@@ -4,21 +4,27 @@ using ResumableFunctions
 function channel_bench()
     Channel() do c
         for i in 1:20
-            put!(c, (i, i+1))
+            put!(c, i)
         end
     end
 end
 
 function list_bench()
-    return [(i, i+1) for i in 1:20]
+    return [i for i in 1:20]
 end
 
 @resumable function generator_bench()
     for i in 1:20
-        @yield (i, i+1)
+        @yield i
     end
 end
 
-@btime channel_bench() # 793.623 ns
-@btime list_bench() # 43.37 ns
-@btime generator_bench() # 4.624 ns
+@btime sum(channel_bench())
+@btime sum(list_bench())
+@btime sum(generator_bench())
+
+#=
+62.987 μs (43 allocations: 1.69 KiB)
+42.855 ns (1 allocation: 240 bytes)
+28.343 ns (1 allocation: 48 bytes)
+=#
