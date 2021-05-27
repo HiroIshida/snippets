816f04d60ff9774218cf0efb682d9c954b74858a
HiroIshida <spitfire.docomo@gmail.com>
2021-05-27 20:47:14 +0900

    Example for ccall func_pointer

--- julia/ccall/call.jl
+++ julia/ccall/call.jl
@@ -1,1 +1,20 @@
-cfunc(x) = ccall((:test, "libmylib.so"), Float64, (Float64,), x)
+const mylib = joinpath(pwd(), "libmylib.so")
+cfunc(x) = ccall((:test, mylib), Float64, (Float64,), x)
+cfunc(2.0)
+cfunc2(f) = ccall((:test_func_pointer, mylib), Cvoid, (Ptr{Cvoid},), f)
+
+function closure()
+    b = 0 
+    function func(x)
+        b = x
+        nothing
+    end
+    function show()
+        println(b)
+    end
+    return func, show
+end
+
+func, show = closure()
+func_c = @cfunction(func, Cvoid, (Cdouble,))
+cfunc2(func_c)
--- julia/ccall/test.c
+++ julia/ccall/test.c
@@ -1,3 +1,7 @@
 double test(double a){
   return a * a;
 }
+
+double test_func_pointer(void (*hoge)(double a)){
+    hoge(114514);
+}
