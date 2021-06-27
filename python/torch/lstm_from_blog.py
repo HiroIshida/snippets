3b38ed48cbd843250d6887c8fa5023be62fa1991
HiroIshida <spitfire.docomo@gmail.com>
2021-06-27 22:43:26 +0900

    lstm from blog

---
+++ python/torch/lstm_from_blog.py
@@ -0,0 +1,194 @@
+# [working example]
+# fetched from here 
+# https://hilinker.hatenablog.com/entry/2018/06/23/204910
+import torch
+import torch.nn as nn
+from torch.optim import SGD
+import math
+import numpy as np
+
+class Predictor(nn.Module):
+    def __init__(self, inputDim, hiddenDim, outputDim):
+        super(Predictor, self).__init__()
+
+        self.rnn = nn.LSTM(input_size = inputDim,
+                            hidden_size = hiddenDim,
+                            batch_first = True)
+        self.output_layer = nn.Linear(hiddenDim, outputDim)
+    
+    def forward(self, inputs, hidden0=None):
+        output, (hidden, cell) = self.rnn(inputs, hidden0)
+        output = self.output_layer(output[:, -1, :])
+
+        return output
+
+def mkDataSet(data_size, data_length=50, freq=60., noise=0.00):
+    """
+    params\n
+    data_size : データセットサイズ\n
+    data_length : 各データの時系列長\n
+    freq : 周波数\n
+    noise : ノイズの振幅\n
+    returns\n
+    train_x : トレーニングデータ（t=1,2,...,size-1の値)\n
+    train_t : トレーニングデータのラベル（t=sizeの値）\n
+    """
+    train_x = []
+    train_t = []
+
+    for offset in range(data_size):
+        train_x.append([[math.sin(2 * math.pi * (offset + i) / freq) + np.random.normal(loc=0.0, scale=noise)] for i in range(data_length)])
+        train_t.append([math.sin(2 * math.pi * (offset + data_length) / freq)])
+
+    return train_x, train_t
+
+def mkRandomBatch(train_x, train_t, batch_size=10):
+    """
+    train_x, train_tを受け取ってbatch_x, batch_tを返す。
+    """
+    batch_x = []
+    batch_t = []
+
+    for _ in range(batch_size):
+        idx = np.random.randint(0, len(train_x) - 1)
+        batch_x.append(train_x[idx])
+        batch_t.append(train_t[idx])
+    
+    return torch.tensor(batch_x), torch.tensor(batch_t)
+
+def main():
+    training_size = 10000
+    test_size = 1000
+    epochs_num = 1000
+    hidden_size = 5
+    batch_size = 100
+
+    train_x, train_t = mkDataSet(training_size)
+    test_x, test_t = mkDataSet(test_size)
+
+    model = Predictor(1, hidden_size, 1)
+    criterion = nn.MSELoss()
+    optimizer = SGD(model.parameters(), lr=0.01)
+
+    for epoch in range(epochs_num):
+        # training
+        running_loss = 0.0
+        training_accuracy = 0.0
+        for i in range(int(training_size / batch_size)):
+            optimizer.zero_grad()
+
+            data, label = mkRandomBatch(train_x, train_t, batch_size)
+
+            output = model(data)
+
+            loss = criterion(output, label)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            training_accuracy += np.sum(np.abs((output.data - label.data).numpy()) < 0.1)
+
+        #test
+        test_accuracy = 0.0
+        for i in range(int(test_size / batch_size)):
+            offset = i * batch_size
+            data, label = torch.tensor(test_x[offset:offset+batch_size]), torch.tensor(test_t[offset:offset+batch_size])
+            output = model(data, None)
+
+            test_accuracy += np.sum(np.abs((output.data - label.data).numpy()) < 0.1)
+        
+        training_accuracy /= training_size
+        test_accuracy /= test_size
+
+        print('%d loss: %.3f, training_accuracy: %.5f, test_accuracy: %.5f' % (
+            epoch + 1, running_loss, training_accuracy, test_accuracy))
+
+
+if __name__ == '__main__':
+    main()
+#Read each module
+import torch
+from torch import nn
+from torch.autograd import Variable
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+   # Random number seed
+torch.manual_seed(1)   
+ 
+# Parameters
+TIME_STEP = 10 # Each batch contains the length of the time series, that is, the amount of data contained
+INPUT_SIZE = 1 # The characteristic length of each data
+LR = 0.02 # learning rate 
+HIDD_NUM = 32 # The number of hidden features
+ 
+ # Define model RNN
+class RNN(nn.Module):
+    def __init__(self):
+        super(RNN, self).__init__()
+ 
+        self.rnn = nn.RNN(
+            input_size=INPUT_SIZE,
+            hidden_size=HIDD_NUM,     # rnn hidden unit
+                         num_layers=3, # RNN layers
+                         batch_first=True, # Put the number of batches first. (batch, time_step, input_size)
+        )
+ 
+        self.fc3 = nn.Linear(HIDD_NUM, 1)
+ 
+    def forward(self, x, h_state):
+        # x (batch, time_step, input_size)
+        # h_state (n_layers, batch, hidden_size)
+        # r_out (batch, time_step, hidden_size)
+        r_out, h_state = self.rnn(x, h_state)
+ 
+        outs = []    # save all predictions
+        for time_step in range(r_out.size(1)):    # calculate output for each time step
+            out = self.fc3(r_out[:, time_step, :])
+            outs.append(out)
+        return torch.stack(outs, dim=1), h_state
+ 
+ # Define model
+rnn = RNN()
+ # Define optimizer and loss function
+optimizer = torch.optim.Adam(rnn.parameters(), lr=LR) 
+loss_func = nn.MSELoss()
+ 
+ # Define hidden state
+h_state = None
+ 
+ # Drawing
+plt.figure(1, figsize=(12, 5))
+plt.ion()           # continuously plot
+ 
+ # Training
+for step in range(2400):
+         # Generate training data
+    start, end = step * np.pi, (step+1)*np.pi   # time range
+    steps = np.linspace(start, end, TIME_STEP, dtype=np.float32)
+    x_np = np.sin(steps)    # float32 for converting torch FloatTensor
+    y_np = np.cos(steps)
+ 
+         # Data is converted to Tensor
+    x = Variable(torch.from_numpy(x_np[np.newaxis, :, np.newaxis]))    # shape (batch, time_step, input_size)
+    y = Variable(torch.from_numpy(y_np[np.newaxis, :, np.newaxis]))
+ 
+    # forward
+    prediction, h_state = rnn(x, h_state)   # rnn output
+    
+         # Since the generated data is not saved, it is not necessary to save the hidden state
+    h_state = Variable(h_state.data)
+    
+    # backward 
+    loss = loss_func(prediction, y)         # cross entropy loss
+    optimizer.zero_grad()                   # clear gradients for this training step
+    loss.backward()                         # backpropagation, compute gradients
+    optimizer.step()                        # apply gradients
+ 
+         # Drawing
+    if step%100==0 or step%101==0:
+        plt.plot(steps, y_np.flatten(), 'r-')
+        plt.plot(steps, prediction.data.numpy().flatten(), 'b-')
+        plt.draw(); plt.pause(0.05)
+ 
+ 
