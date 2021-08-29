2a54bda0bb7723f5556817c146874bd3491365e3
HiroIshida <spitfire.docomo@gmail.com>
2021-08-30 03:01:17 +0900

    Add new axis

--- python/torch/parameter_manipulation.py
+++ python/torch/parameter_manipulation.py
@@ -21,5 +21,19 @@ def repeat_case():
     loss.backward()
     print(p.grad)
 
+def repeat_and_add_axis_case():
+    p = torch.nn.Parameter(torch.zeros(2))
+    # P share the smae p through the repeated components
+    P = p.repeat(3, 1)
+    P_aug = P[None, :, :]
+    y_aug = y[None, :, :]
+    loss = torch.nn.MSELoss(reduction='sum')(y_aug, P_aug)
+    loss.backward()
+    print(p.grad)
+
+print("check that all gradients computed via different ways are equals")
 loop_case()
 repeat_case()
+repeat_and_add_axis_case()
+
+# parameter repeat and reshape?
