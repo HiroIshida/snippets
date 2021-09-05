8e317194b90f3a7f9209e65940e1e6ac20301ce4
HiroIshida <spitfire.docomo@gmail.com>
2021-09-05 16:52:30 +0900

    Check minibatch propagation

--- python/torch/splitting_lstm.py
+++ python/torch/splitting_lstm.py
@@ -7,16 +7,23 @@ from torch import nn
 torch.manual_seed(0) 
 n_dim = 3
 n_hid = 10
+n_seq = 100
 model = nn.LSTM(n_dim, n_hid, batch_first=True)
 
 # batch propagation
-seq = torch.rand(1, 10, n_dim)
+seq = torch.rand(1, n_seq, n_dim)
 out_batch, hc_tuple_batch = model(seq, None)
+
+# mini-batch propagation
+mini_seqs = seq.split(30, dim=1)
+hc_tuple_minimbatch = None
+for mini_seq in mini_seqs:
+    out, hc_tuple_minimbatch = model(mini_seq, hc_tuple_minimbatch)
 
 # sequencial propagation
 hc_tuple = None
 out_batch_emurate = []
-for i in range(10):
+for i in range(n_seq):
     x = seq[:, torch.tensor([i]), :]
     out, hc_tuple = model(x, hc_tuple)
     out_batch_emurate.append(out)
@@ -24,6 +31,7 @@ out_batch_emurate = torch.cat(out_batch_emurate, 1)
 
 # compare
 assert torch.all(torch.isclose(out_batch, out_batch_emurate))
-for i in range(2):
-    assert torch.all(torch.isclose(hc_tuple_batch[i], hc_tuple[i]))
-
+for hc_tuple_testing in [hc_tuple, hc_tuple_minimbatch]:
+    for i in range(2):
+        diff = hc_tuple_batch[i] - hc_tuple_testing[i]
+        assert torch.all(torch.isclose(hc_tuple_batch[i], hc_tuple_testing[i]))
