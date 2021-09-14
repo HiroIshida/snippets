484712f623f4a934c80e5b6b22fe863f817ab6a4
HiroIshida <spitfire.docomo@gmail.com>
2021-09-15 05:45:08 +0900

    Server client

---
+++ ros2/server_client/client.py
@@ -0,0 +1,53 @@
+import sys
+import time
+
+from example_interfaces.srv import AddTwoInts
+import rclpy
+from rclpy.node import Node
+
+
+class MinimalClientAsync(Node):
+
+    def __init__(self):
+        super().__init__('minimal_client_async')
+        self.cli = self.create_client(AddTwoInts, 'add_two_ints')
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.req = AddTwoInts.Request()
+
+    def send_request(self):
+        self.req.a = int(3)
+        self.req.b = int(2)
+        self.future = self.cli.call_async(self.req)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_client = MinimalClientAsync()
+
+    ts = time.time()
+    minimal_client.send_request()
+
+    while rclpy.ok():
+        rclpy.spin_once(minimal_client)
+        if minimal_client.future.done():
+            print("elapsed {}".format(time.time() - ts))
+            te = time.time()
+            try:
+                response = minimal_client.future.result()
+            except Exception as e:
+                minimal_client.get_logger().info(
+                    'Service call failed %r' % (e,))
+            else:
+                minimal_client.get_logger().info(
+                    'Result of add_two_ints: for %d + %d = %d' %
+                    (minimal_client.req.a, minimal_client.req.b, response.sum))
+            break
+
+    minimal_client.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
