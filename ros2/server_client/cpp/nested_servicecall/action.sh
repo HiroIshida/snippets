fe403fd189dd56b5e846614adcfa9c6a3cbd49d4
HiroIshida <spitfire.docomo@gmail.com>
2021-09-27 13:26:20 +0900

    Renamed

---
+++ ros2/server_client/cpp/nested_servicecall/action.sh
@@ -0,0 +1,2 @@
+cd ~/colcon_ws
+colcon build --packages-select nested_servicecall --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=1
--- ros2/server_client/cpp/sample/action.sh
+++
@@ -1,2 +0,0 @@
-cd ~/colcon_ws
-colcon build --packages-select sample --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=1
