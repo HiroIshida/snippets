3eca519d9abbb8646a21da88eb6d3ffe3fad1c07
HiroIshida <spitfire.docomo@gmail.com>
2021-11-02 03:30:35 +0900

    Train dense prop

---
+++ python/ext_examples/mimic/train_dense_prop.py
@@ -0,0 +1,63 @@
+import argparse
+import sys
+import typing
+from typing import Union
+
+import torch
+
+from mimic.trainer import Config
+from mimic.trainer import TrainCache
+from mimic.trainer import train
+from mimic.datatype import AbstractDataChunk
+from mimic.datatype import ImageDataChunk
+from mimic.datatype import ImageCommandDataChunk
+from mimic.dataset import AutoRegressiveDataset
+from mimic.dataset import BiasedAutoRegressiveDataset
+from mimic.models import LSTMConfig
+from mimic.models import LSTM
+from mimic.models import BiasedLSTM
+from mimic.models import DenseConfig
+from mimic.models import DenseProp
+from mimic.models import ImageAutoEncoder
+from mimic.models import BiasedDenseProp
+
+from mimic.scripts.utils import split_with_ratio
+from mimic.scripts.utils import create_default_logger
+from mimic.scripts.utils import query_yes_no
+from mimic.scripts.train_propagator import prepare_trained_image_chunk
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-mode', type=int, default=1)
+    args = parser.parse_args()
+    mode = args.mode
+
+    project_name = 'kuka_reaching'
+    logger = create_default_logger(project_name, 'propagator_mode{}'.format(mode))
+    chunk: ImageCommandDataChunk = prepare_trained_image_chunk(project_name)
+    dataset = BiasedAutoRegressiveDataset.from_chunk(chunk)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    if mode==1:
+        dataset = AutoRegressiveDataset.from_chunk(chunk)
+        prop_model = LSTM(device, dataset.n_state, LSTMConfig())
+        tcache = TrainCache[LSTM](project_name, LSTM)
+    elif mode==2:
+        dataset = BiasedAutoRegressiveDataset.from_chunk(chunk)
+        prop_model = BiasedLSTM(device, dataset.n_state, dataset.n_bias, LSTMConfig())
+        tcache = TrainCache[BiasedLSTM](project_name, BiasedLSTM)
+    else:
+        dataset = BiasedAutoRegressiveDataset.from_chunk(chunk)
+        if mode==3:
+            model_config = DenseConfig(200, 6)
+        elif mode==4:
+            model_config = DenseConfig(200, 6, 'relu')
+        elif mode==5:
+            model_config = DenseConfig(200, 6, 'sigmoid')
+        else:
+            sys.exit()
+        prop_model = BiasedDenseProp(device, dataset.n_state, dataset.n_bias, model_config)
+        tcache = TrainCache[BiasedDenseProp](project_name, BiasedDenseProp)
+
+    ds_train, ds_valid = split_with_ratio(dataset)
+    train(prop_model, ds_train, ds_valid, tcache=tcache, config=Config(n_epoch=3000))
