634d2fa73ca2bcfffab23eee2a512b318b020ead
HiroIshida <spitfire.docomo@gmail.com>
2021-11-03 21:28:56 +0900

    Update eval

--- python/ext_examples/mimic/analyze_prop_models.py
+++ python/ext_examples/mimic/analyze_prop_models.py
@@ -1,5 +1,5 @@
-from mimic.models import ImageAutoEncoder, LSTM, DenseProp
-from mimic.dataset import AutoRegressiveDataset
+from mimic.models import ImageAutoEncoder, LSTM, DenseProp, DeprecatedDenseProp
+from mimic.dataset import AutoRegressiveDataset, FirstOrderARDataset
 from mimic.trainer import TrainCache
 from mimic.predictor import evaluate_command_prediction_error
 from mimic.scripts.train_propagator import prepare_trained_image_chunk
@@ -8,15 +8,17 @@ project_name = 'kuka_reaching'
 n_intact = 5
 chunk = prepare_trained_image_chunk(project_name)
 chunk_intact, _ = chunk.split(n_intact)
-dataset = AutoRegressiveDataset.from_chunk(chunk_intact)
 
 tcache_ae = TrainCache[ImageAutoEncoder].load(project_name, ImageAutoEncoder)
 tcache_lstm = TrainCache[LSTM].load(project_name, LSTM)
 tcache_dense = TrainCache[DenseProp].load(project_name, DenseProp)
+tcache_depre = TrainCache[DeprecatedDenseProp].load(project_name, DeprecatedDenseProp)
 
-val_lstm = evaluate_command_prediction_error(tcache_ae.best_model, tcache_lstm.best_model, dataset)
+val_lstm = evaluate_command_prediction_error(tcache_ae.best_model, tcache_lstm.best_model, chunk_intact)
 print(val_lstm)
 
-val_dense = evaluate_command_prediction_error(tcache_ae.best_model, tcache_dense.best_model, dataset)
+val_dense = evaluate_command_prediction_error(tcache_ae.best_model, tcache_dense.best_model, chunk_intact)
 print(val_dense)
 
+val_dense = evaluate_command_prediction_error(tcache_ae.best_model, tcache_depre.best_model, chunk_intact)
+print(val_dense)
