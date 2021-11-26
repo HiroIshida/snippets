cdb936c046b24de69248a0c32003cb5c9510bb9b
HiroIshida <spitfire.docomo@gmail.com>
2021-11-26 09:31:41 +0000

    Update train script

--- python/ext_examples/mimic/train_real_robot.sh
+++ python/ext_examples/mimic/train_real_robot.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 python3 -m mimic.scripts.train_auto_encoder -pn real_robot -n 5000 -bottleneck 60
-#python3 -m mimic.scripts.train_propagator -pn real_robot -n 4000 -model LSTM
-#python3 -m mimic.scripts.train_propagator -pn real_robot -n 4000 -model LSTM
-#python3 -m mimic.scripts.train_propagator -pn real_robot -n 4000 -model LSTM
-#python3 -m mimic.scripts.train_propagator -pn real_robot -n 4000 -model LSTM
+python3 -m mimic.scripts.train_propagator -pn real_robot -n 4000 -model LSTM -daug 30 -covscale 0.1
