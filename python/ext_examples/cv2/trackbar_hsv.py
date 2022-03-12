55a471448f11db4fdb698347e42c1976f566e6cf
HiroIshida <spitfire.docomo@gmail.com>
2022-03-12 14:08:14 +0900

    Trackbar hsv

--- python/ext_examples/cv2/trackbar_hsv.py
+++ python/ext_examples/cv2/trackbar_hsv.py
@@ -4,8 +4,10 @@ import cv2
 import numpy as np
 from typing import List
 
+
 @dataclass
 class SingleFilter:
+    name: str
     b_min: int
     b_max: int
     idx: int
@@ -14,20 +16,42 @@ class SingleFilter:
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         return np.logical_and(hsv[:, :, self.idx] >= self.b_min, hsv[:, :, self.idx] <= self.b_max)
 
+    def track_bar_min_name(self):
+        return self.name + '_min'
+
+    def track_bar_max_name(self):
+        return self.name + '_max'
+
+    def __post_init__(self):
+        print(self.track_bar_min_name())
+        cv2.createTrackbar(self.track_bar_min_name(), "window", 0, 255, lambda x: None)
+        cv2.createTrackbar(self.track_bar_max_name(), "window", 0, 255, lambda x: None)
+
+    def reflect_trackbar(self):
+        self.b_min = cv2.getTrackbarPos(self.track_bar_min_name(), "window")
+        self.b_max = cv2.getTrackbarPos(self.track_bar_max_name(), "window")
+
 
 class HSVFilter:
     BLACK = (0, 0, 0)
     bound_list: List[SingleFilter]
 
     def __init__(self):
-        h_bound = SingleFilter(0, 255, 0)
-        s_bound = SingleFilter(0, 255, 1)
-        v_bound = SingleFilter(0, 255, 2)
+        cv2.namedWindow('window')
+        h_bound = SingleFilter('h', 0, 255, 0)
+        s_bound = SingleFilter('s', 0, 255, 1)
+        v_bound = SingleFilter('v', 0, 255, 2)
         self.bound_list = [h_bound, s_bound, v_bound]
 
     def get_logical(self, img: np.ndarray):
-        logical = np.logical_and(*[bound.get_logical(img) for bound in self.bound_list])
-        return logical
+        a = self.bound_list[0].get_logical(img)
+        b = self.bound_list[1].get_logical(img)
+        c = self.bound_list[2].get_logical(img)
+        return a * b * c
+    
+    def reflect_trackbar(self):
+        for bound in self.bound_list:
+            bound.reflect_trackbar()
 
     def __call__(self, img: np.ndarray, fill_value=BLACK):
         fill_indices = np.logical_not(self.get_logical(img))
@@ -38,11 +62,10 @@ class HSVFilter:
 
 if __name__ == '__main__':
     img = cv2.imread('data/cook.jpg')
-    cv2.namedWindow('window')
 
     hsvfilter = HSVFilter()
-    img_filtered = hsvfilter(img)
 
-    import matplotlib.pyplot as plt 
-    plt.imshow(img_filtered)
-    plt.show()
+    while True:
+        cv2.imshow('window', hsvfilter(img))
+        hsvfilter.reflect_trackbar()
+        cv2.waitKey(10)
