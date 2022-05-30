d403da64d10b83e9ae69459c9e40056e21cc1a32
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2022-05-30 10:39:04 +0900

    Take a hash snapshot of vcs

---
+++ misc/vcstools/snapshot.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+import copy
+from typing import List
+import os
+import subprocess
+import yaml
+
+base_dir = '/home/h-ishida/pilot-auto'
+filename = os.path.join(base_dir, 'autoware.repos')
+filename_out = os.path.join(base_dir, 'autoware.repos-snapshot')
+
+with open(filename, 'r') as f:
+    data = yaml.safe_load(f)
+
+excludes = ['autoware/universe', 'autoware/launcher']
+
+repos = data['repositories']
+for repo_name, repo in data['repositories'].items():
+    if repo_name in excludes:
+        continue
+
+    assert repo['type'] == 'git'
+    repo_localition = os.path.join(base_dir, 'src', repo_name)
+    cmd = ['git', '-C', repo_localition, 'rev-parse', '--verify', 'HEAD']
+    print('processing {}'.format(repo_name))
+    out = subprocess.run(cmd, capture_output=True)
+    repo['version'] = out.stdout.decode().strip()
+
+with open(filename_out, 'w') as f:
+    yaml.dump(data, f)
