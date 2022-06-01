fc63446a497b53595c8e084c5307dc2ad5baa7c3
HiroIshida <spitfire.docomo@gmail.com>
2022-06-01 10:07:20 +0900

    Compare with rpy

--- python/ext_examples/sympy/rpy_matrix/rpy_matrix.py
+++ python/ext_examples/sympy/rpy_matrix/rpy_matrix.py
@@ -3,7 +3,7 @@ import numpy as np
 import sympy
 from sympy import Matrix
 from sympy.vector.orienters import AxisOrienter
-from skrobot.coordinates.math import rotation_matrix
+from skrobot.coordinates.math import rpy_matrix
 
 
 def expr_rotation_x(theta):
@@ -39,25 +39,24 @@ arg_exprs = sympy.symbols(arg_names)
 rpy_mat_expr = expr_rotation_matrix(*arg_exprs)
 x_axis_expr = rpy_mat_expr[:, 0]
 jac_expr = x_axis_expr.jacobian(arg_exprs)
-print(jac_expr)
+f_rpy_matrix = sympy.lambdify(arg_exprs, rpy_mat_expr, modules="numpy")
+
+np.testing.assert_almost_equal(f_rpy_matrix(0.4, 0.3, 0.2), rpy_matrix(0.2, 0.3, 0.4), decimal=8)
 
 f_jac_axis = sympy.lambdify(arg_exprs, jac_expr, modules="numpy")
 jac = f_jac_axis(0.2, 0.2, 0.2)
-print(jac)
 
-def compute_by_hand(roll, pitch, yaw):
+def rpy_compute_by_hand(roll, pitch, yaw):
     jac =  np.array([
         [0, -sin(pitch)*cos(yaw), -sin(yaw)*cos(pitch)],
         [0, -sin(pitch)*sin(yaw),  cos(pitch)*cos(yaw)],
         [0,          -cos(pitch),                    0]])
     return jac
 
-
-# check computation time
 import time
 ts = time.time()
 for _ in range(10000):
-    compute_by_hand(0.2, 0.2, 0.2)
+    rpy_compute_by_hand(0.2, 0.2, 0.2)
 print("compute by hand: {} sec".format(time.time() - ts))
 
 ts = time.time()
