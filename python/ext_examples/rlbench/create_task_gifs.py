033e5895ae0dbeeff552c59712d09725a7e47ffb
HiroIshida <spitfire.docomo@gmail.com>
2022-06-13 16:12:03 +0900

    Revert "Use multiprocess"
    This reverts commit a2b21a7294159047e28d7624a2be817c7bcc6f15.

--- python/ext_examples/rlbench/create_task_gifs.py
+++ python/ext_examples/rlbench/create_task_gifs.py
@@ -1,53 +1,72 @@
+import argparse
+import inspect
+from pathlib import Path
+from typing import Type
+
+import numpy as np
+import rlbench.tasks
+import tqdm
+from moviepy.editor import ImageSequenceClip
+from rlbench.action_modes.action_mode import MoveArmThenGripper
+from rlbench.action_modes.arm_action_modes import JointVelocity
+from rlbench.action_modes.gripper_action_modes import Discrete
+from rlbench.backend.task import Task
+from rlbench.demo import Demo
+from rlbench.environment import Environment
+from rlbench.observation_config import ObservationConfig
+
+from typing import List
+
+
+def rlbench_demo_to_rgb_seq(demo: Demo) -> List[np.ndarray]:
+    seq = []
+    for obs in demo:
+        rgbs = []
+        for camera_name in ["overhead", "left_shoulder", "right_shoulder", "front", "wrist"]:
+            rgbs.append(obs.__dict__[camera_name + "_rgb"])
+        rgb = np.concatenate(rgbs, axis=1)
+        seq.append(rgb)
+    return seq
+
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-pn", type=str, default="rlbench_close_box", help="project name"
+    )
+    parser.add_argument("-tn", type=str, default="CloseDrawer", help="task name")
+    parser.add_argument("-cn", type=str, default="overhead", help="camera name")
+    parser.add_argument("-resol", type=int, default=112, help="epoch num")
+    args = parser.parse_args()
+    project_name = args.pn
+    resolution = args.resol
 
-    def task_process(task_class):
+    assert resolution in [112, 224]
 
-        from pathlib import Path
-        from typing import List
+    # Data generation by rlbench
+    obs_config = ObservationConfig()
+    obs_config.set_all(True)
 
-        import numpy as np
-        import tqdm
-        from moviepy.editor import ImageSequenceClip
+    env = Environment(
+        action_mode=MoveArmThenGripper(
+            arm_action_mode=JointVelocity(), gripper_action_mode=Discrete()
+        ),
+        obs_config=ObservationConfig(),
+        headless=True,
+    )
+    env.launch()
 
-        from rlbench.action_modes.action_mode import MoveArmThenGripper
-        from rlbench.action_modes.arm_action_modes import JointVelocity
-        from rlbench.action_modes.gripper_action_modes import Discrete
-        from rlbench.demo import Demo
-        from rlbench.environment import Environment
-        from rlbench.observation_config import ObservationConfig
+    all_task_classes = []
+    for attr in rlbench.tasks.__dict__.values():
+        if not inspect.isclass(attr):
+            continue
+        if issubclass(attr, Task):
+            all_task_classes.append(attr)
 
-        def rlbench_demo_to_rgb_seq(demo: Demo) -> List[np.ndarray]:
-            seq = []
-            for obs in demo:
-                rgbs = []
-                for camera_name in [
-                    "overhead",
-                    "left_shoulder",
-                    "right_shoulder",
-                    "front",
-                    "wrist",
-                ]:
-                    rgbs.append(obs.__dict__[camera_name + "_rgb"])
-                rgb = np.concatenate(rgbs, axis=1)
-                seq.append(rgb)
-            return seq
+    dirpath = Path("./rlbench_gif")
+    dirpath.mkdir(exist_ok=True)
 
-        obs_config = ObservationConfig()
-        obs_config.set_all(True)
-
-        env = Environment(
-            action_mode=MoveArmThenGripper(
-                arm_action_mode=JointVelocity(), gripper_action_mode=Discrete()
-            ),
-            obs_config=ObservationConfig(),
-            headless=True,
-        )
-        env.launch()
-
-        resolution = 112
-        dirpath = Path("./rlbench_gif")
-        dirpath.mkdir(exist_ok=True)
-
+    for task_class in all_task_classes:
         print("executing task class named {}".format(task_class.__name__))
         task = env.get_task(task_class)
 
@@ -58,20 +77,3 @@ if __name__ == "__main__":
         clip = ImageSequenceClip([img for img in rgb_seq], fps=50)
         file_path = dirpath / (task_class.__name__ + ".gif")
         clip.write_gif(str(file_path), fps=50)
-
-    import inspect
-    import os
-    from multiprocessing import Pool
-
-    import rlbench.tasks
-    from rlbench.backend.task import Task
-
-    all_task_classes = []
-    for attr in rlbench.tasks.__dict__.values():
-        if not inspect.isclass(attr):
-            continue
-        if issubclass(attr, Task):
-            all_task_classes.append(attr)
-
-    p = Pool(int(os.cpu_count() * 0.5))
-    p.map(task_process, all_task_classes)
