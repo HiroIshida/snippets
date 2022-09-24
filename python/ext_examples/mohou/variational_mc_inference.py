37e45b598ef21e1b69ca2580ad72c278e53b12cf
HiroIshida <spitfire.docomo@gmail.com>
2022-09-25 06:18:17 +0900

    Update variational mc

--- python/ext_examples/mohou/variational_mc_inference.py
+++ python/ext_examples/mohou/variational_mc_inference.py
@@ -1,35 +1,35 @@
-import torch
 import numpy as np
+import torch
 import tqdm
-from mohou.types import AngleVector, EpisodeBundle
-from mohou.trainer import TrainCache
-from mohou.model.lstm import LSTM
+
+import matplotlib.pyplot as plt
+from mohou.encoding_rule import EncodingRule
 from mohou.file import get_project_path
-from mohou.default import create_default_propagator, create_default_encoding_rule
-from mohou.propagator import Propagator
-from mohou.propagator import Propagator
+from mohou.model.lstm import LSTM
 from mohou.model.third_party.variational_lstm import VariationalLSTM
+from mohou.propagator import LSTMPropagator
+from mohou.trainer import TrainCache
+from mohou.types import AngleVector, EpisodeBundle
 
 pp = get_project_path("pr2_tidyup_dish")
 bundle = EpisodeBundle.load(pp)
 
-rule = create_default_encoding_rule(pp)
+rule = EncodingRule.create_default(pp)
 episode = bundle[5]
-#episode = bundle.get_untouch_bundle()[0]
+# episode = bundle.get_untouch_bundle()[0]
 
 lstm = TrainCache.load(pp, LSTM, variational=True).best_model
 vlstm: VariationalLSTM = lstm.lstm_layer  # type: ignore
-prop = Propagator(lstm, rule, device=torch.device("cpu"))
-#prop = Propagator(lstm, rule)
+prop = LSTMPropagator(lstm, rule, device=torch.device("cpu"))
 
 av_seq_list = []
-n_mc = 30
+n_mc = 1000
 for _ in tqdm.tqdm(range(n_mc)):
     prop.reset()
     av_list = []
     for i in range(len(episode)):
         edict = episode[i]
-        if i==0:
+        if i == 0:
             av_list.append(edict[AngleVector].numpy())
         prop.feed(edict)
         vlstm.randomize()
@@ -43,10 +43,9 @@ cov_list = []
 det_list = []
 for t in range(len(episode)):
     avs = av_seq_arr[:, t, :]
-    cov =  np.cov(avs.T)
+    cov = np.cov(avs.T)
     cov_list.append(cov)
     det_list.append(np.linalg.det(cov))
 
-import matplotlib.pyplot as plt
 plt.plot(np.log(det_list[1:]))
 plt.show()
