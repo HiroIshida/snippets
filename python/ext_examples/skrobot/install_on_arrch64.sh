d54866c57baa1fa7835153da94f566e59579f986
HiroIshida <spitfire.docomo@gmail.com>
2022-11-08 03:18:03 +0900

    Add install instruction on aarch64

---
+++ python/ext_examples/skrobot/install_on_arrch64.sh
@@ -0,0 +1,7 @@
+sudo apt install liblapack-dev libopenblas-dev
+sudo apt install libsuitesparse-dev 
+export CPPFLAGS="-I/usr/include/suitesparse" 
+pip3 install cvxopt
+
+python-fcl
+sudo apt-get install libfcl-dev liboctomap-dev
