e5f284c27da7d53e81f5709203f7c615a895a131
HiroIshida <spitfire.docomo@gmail.com>
2022-12-11 10:21:11 +0900

    pycall finish

--- euslisp/pycall/wrapper.cpp
+++ euslisp/pycall/wrapper.cpp
@@ -4,28 +4,30 @@
 
 using namespace std;
 
-void*  __PyImport_ImportModule(const char* module_name){
+long  __PyImport_ImportModule(const char* module_name){
   Py_Initialize();
   PyObject* module = PyImport_ImportModule(module_name);
   char* name = PyModule_GetName(module);
   std::cout << "[pywrap] imported : " << name << std::endl; 
   char* filepath = PyModule_GetFilename(module);
   std::cout << "[pywrap] import path : " << filepath << std::endl; 
-  return (void*)module;
+  std::cout << (long)((void*)module) << std::endl;
+  return (long)((void*)module);
 }
 
-long __PyObject_CallMethod(void* module_, const char* module_method){
-  Py_Initialize();
-  auto module = (PyObject*)module_;
-  PyObject_CallMethod(module, const_cast<char*>(module_method), NULL);
-  return 0;
+long __PyObject_CallMethod(long module_, const char* module_method){
+  // Py_Initialize();
+  auto module = (PyObject*)((void*)module_);
+  std::cout << module_method << std::endl;
+  PyObject_CallMethod(module, "exit", NULL);
+  return -1;
 }
 
 extern "C" {
-    void* _PyImport_ImportModule(const char* module_name){
+    long _PyImport_ImportModule(const char* module_name){
         return __PyImport_ImportModule(module_name);
     }
-    long _PyObject_CallMethod(void* module_, const char* module_method){
+    long _PyObject_CallMethod(long module_, const char* module_method){
         return __PyObject_CallMethod(module_, module_method);
     }
 }
