aee49481f1b6771910555d6208c8b1e74cd96348
HiroIshida <spitfire.docomo@gmail.com>
2023-10-16 17:30:25 +0900

    LQR control also x_dot

--- python/small_impls/cartpole/cartpole.py
+++ python/small_impls/cartpole/cartpole.py
@@ -37,11 +37,11 @@ class Cartpole:
 
     def is_uplight(self) -> bool:
         x, x_dot, theta, theta_dot = self.state
-        return abs(np.cos(theta) - (-1)) < 0.3 and abs(theta_dot) < 0.5
+        return abs(np.cos(theta) - (-1)) < 0.3 and abs(theta_dot) < 0.3
 
     def is_static(self) -> bool:
         x, x_dot, theta, theta_dot = self.state
-        return abs(np.cos(theta) - (-1)) < 0.03 and abs(theta_dot) < 0.01
+        return abs(np.cos(theta) - (-1)) < 0.01 and abs(theta_dot) < 0.01 and abs(x_dot) < 0.01
 
 
 
@@ -59,7 +59,7 @@ class CartpoleVisualizer:
         self.cart = ax.add_patch(cart_circle)
         pole_line = plt.Line2D((0, 0), (0, - model_param.l), color="black")
         self.pole = ax.add_line(pole_line)
-        ax.set_xlim(-8, 8)
+        ax.set_xlim(-12, 12)
         ax.set_ylim(-2, 2)
         ax.set_aspect('equal')
 
@@ -81,30 +81,46 @@ class LQRController:
     def __init__(self, model_param: ModelParameter):
         m, M, l, g = model_param.m, model_param.M, model_param.l, model_param.g
 
+        def x_acc(theta, theta_dot, u) -> float:
+            return (u + m * np.sin(theta) * (l * theta_dot ** 2 + g * np.cos(theta))) / (M + m * np.sin(theta) ** 2)
+
         def theta_acc(theta, theta_dot, u) -> float:
             x_acc = (u + m * np.sin(theta) * (l * theta_dot ** 2 + g * np.cos(theta))) / (M + m * np.sin(theta) ** 2)
             theta_acc = -1 * (np.cos(theta) * x_acc + g * np.sin(theta)) / l
             return theta_acc
 
         eps = 1e-6
-        A = np.zeros((2, 2))
-        A[0, 0] = 0
-        A[0, 1] = 1.0
-        A[1, 0] = (theta_acc(np.pi + eps, 0.0, 0.0) - theta_acc(np.pi, 0.0, 0.0)) / eps
-        A[1, 1] = (theta_acc(np.pi, eps, 0.0) - theta_acc(np.pi, 0.0, 0.0)) / eps
-        B = np.zeros((2, 1))
-        B[1, 0] = (theta_acc(np.pi, 0.0, 0.0 + eps) - theta_acc(np.pi, 0.0, 0.0)) / eps
+        A_x = np.zeros((1, 2))
+        A_x[0, 0] = (x_acc(np.pi + eps, 0.0, 0.0) - x_acc(np.pi, 0.0, 0.0)) / eps
+        A_x[0, 1] = (x_acc(np.pi, eps, 0.0) - x_acc(np.pi, 0.0, 0.0)) / eps
+        b_x = (x_acc(np.pi, 0.0, 0.0 + eps) - x_acc(np.pi, 0.0, 0.0)) / eps
+
+        A_theta = np.zeros((2, 2))
+        A_theta[0, 0] = 0
+        A_theta[0, 1] = 1.0
+        A_theta[1, 0] = (theta_acc(np.pi + eps, 0.0, 0.0) - theta_acc(np.pi, 0.0, 0.0)) / eps
+        A_theta[1, 1] = (theta_acc(np.pi, eps, 0.0) - theta_acc(np.pi, 0.0, 0.0)) / eps
+        B_theta = np.zeros((2, 1))
+        B_theta[1, 0] = (theta_acc(np.pi, 0.0, 0.0 + eps) - theta_acc(np.pi, 0.0, 0.0)) / eps
+
+        A = np.zeros((3, 3))
+        B = np.zeros((3, 1))
+        A[:2, :2] = A_theta
+        A[2, :2] = A_x
+        B[:2] = B_theta
+        B[2] = b_x
+
         self.A = A
         self.B = B
 
     def __call__(self, state: np.ndarray):
         x, x_dot, theta, theta_dot = state
-        Q = np.eye(2)
+        Q = np.eye(3)
         R = np.eye(1) * 0.1
         K, _, _ = lqr(self.A, self.B, Q, R)
         target_cand = [-3 * np.pi, -np.pi, np.pi, 3 * np.pi]
         target = min(target_cand, key=lambda x: abs(x - theta))
-        u = -K @ np.array([theta - target, theta_dot])
+        u = -K @ np.array([theta - target, theta_dot, x_dot])
         return u
 
 
@@ -131,9 +147,10 @@ class EnergyShapingController:
 if __name__ == "__main__":
     model_actual = ModelParameter()
     model_est = copy.deepcopy(model_actual)
-    model_est.m = 0.98
-    system = Cartpole(np.array([0.0, 0.0, 0.3, 0.0]))
-    controller = EnergyShapingController(model_est)
+    # model_est.m = 1.1
+    # model_est.M = 0.8
+    system = Cartpole(np.array([0.0, 0.0, 0.1, 0.0]))
+    controller = EnergyShapingController(model_est, 0.2)
     lqr_controller = LQRController(model_est)
     f_history = []
 
@@ -152,11 +169,12 @@ if __name__ == "__main__":
         if system.is_static():
             print("static")
             break
+    print(len(system.history))
 
     vis = CartpoleVisualizer(model_actual)
     for state in system.history:
         vis.render(state)
-
+    import time; time.sleep(1000)
     # history = np.array(system.history)
     # plt.plot(history[:, 2], history[:, 3])
     # plt.plot(history[-1, 2], history[-1, 3], "o", markersize=10)
