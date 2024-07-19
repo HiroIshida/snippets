ec49adaa8b82595746baf7a238e985dd8117c105
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-07-20 01:57:29 +0900

    Minimum example to train policy (on random data)

--- python/ext_examples/lerobot/build_data.py
+++ python/ext_examples/lerobot/build_data.py
@@ -23,14 +23,16 @@ import torchvision
 from lerobot.common.datasets.lerobot_dataset import LeRobotDataset
 from lerobot.common.policies.diffusion.configuration_diffusion import DiffusionConfig
 from lerobot.common.policies.diffusion.modeling_diffusion import DiffusionPolicy
+from lerobot.common.datasets.compute_stats import compute_stats
 
+@dataclass
 class DummyEpisode:
     images: np.ndarray
     states: np.ndarray
     actions: np.ndarray
-    video_path: Path
 
-    def __init__(self, T: int):
+    @classmethod
+    def create(cls, T: int):
         image_list = []
         state_list = []
         action_list = []
@@ -41,12 +43,17 @@ class DummyEpisode:
             image_list.append(rand_image)
             state_list.append(rand_state)
             action_list.append(rand_action)
-        self.images = np.array(image_list)
-        self.states = np.array(state_list)
-        self.actions = np.array(action_list)
+        images = np.array(image_list)
+        states = np.array(state_list)
+        actions = np.array(action_list)
+        return cls(images, states, actions)
 
 
-def convert_to_data_dict(episodes: List[DummyEpisode], fps: int) -> LeRobotDataset:
+def convert_to_lerobot_dataset(
+    episodes: List[DummyEpisode],
+    fps: int,
+    batch_size: int = 32,
+    num_workers: int = 8) -> LeRobotDataset:
     # copied and tweaked from
     # https://github.com/ojh6404/imitator/blob/lerobot/imitator/scripts/lerobot_dataset_builder.py
 
@@ -58,7 +65,7 @@ def convert_to_data_dict(episodes: List[DummyEpisode], fps: int) -> LeRobotDatas
         dones[-1] = True
 
         ep_dict = {}
-        ep_dict["observation.images.camera"] = [PILImage.fromarray(im) for im in ep.images]
+        ep_dict["observation.images"] = [PILImage.fromarray(im) for im in ep.images]
         ep_dict["observation.state"] = torch.from_numpy(ep.states).float()
         ep_dict["action"] = torch.from_numpy(ep.actions).float()
         ep_dict["episode_index"] = torch.ones(T, dtype=torch.int64) * idx
@@ -75,7 +82,7 @@ def convert_to_data_dict(episodes: List[DummyEpisode], fps: int) -> LeRobotDatas
     dim_action = len(episodes[0].actions[0])
     features = Features(
         {
-            "observation.images.camera": Image(),
+            "observation.images": Image(),
             "observation.state": Sequence(Value("float32"), length=dim_state),
             "action": Sequence(Value("float32"), length=dim_action),
             "episode_index": Value("int64"),
@@ -98,23 +105,27 @@ def convert_to_data_dict(episodes: List[DummyEpisode], fps: int) -> LeRobotDatas
         episode_data_index=episode_data_index,
         info=info,
     )
+    print("compute stats")
+    stats = compute_stats(lerobot_dataset, batch_size, num_workers)
+    lerobot_dataset.stats = stats
     return lerobot_dataset
 
 
 if __name__ == "__main__":
     episode_list = []
     for _ in range(30):
-        episode_list.append(DummyEpisode(80))
-    dataset = convert_to_data_dict(episode_list, 1)
+        episode_list.append(DummyEpisode.create(80))
+    dataset = convert_to_lerobot_dataset(episode_list, 1)
+    delta_timestamps = {
+        "observation.images": [-0.1, 0.0],
+        "observation.state": [-0.1, 0.0],
+        "action": [-0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4],
+    }
+    dataset.delta_timestamps = delta_timestamps
 
     training_steps = 5000
     device = torch.device("cuda")
     log_freq = 250
-    delta_timestamps = {
-        "observation.image": [-0.1, 0.0],
-        "observation.state": [-0.1, 0.0],
-        "action": [-0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4],
-    }
 
     cfg = DiffusionConfig()
     policy = DiffusionPolicy(cfg, dataset_stats=dataset.stats)
@@ -123,7 +134,6 @@ if __name__ == "__main__":
 
     optimizer = torch.optim.Adam(policy.parameters(), lr=1e-4)
 
-    # Create dataloader for offline training.
     dataloader = torch.utils.data.DataLoader(
         dataset,
         num_workers=4,
@@ -133,7 +143,6 @@ if __name__ == "__main__":
         drop_last=True,
     )
 
-    # Run training loop.
     step = 0
     done = False
     while not done:
