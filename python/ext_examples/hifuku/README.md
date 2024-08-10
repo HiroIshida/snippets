b8d471c791f2baf75f51848de3735f1496e1bb26
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-08-10 22:44:25 +0900

    Add hifuku application to fetch tidyup

---
+++ python/ext_examples/hifuku/README.md
@@ -0,0 +1,10 @@
+## Installation
+please follow instruction of [hifuku](https://github.com/HiroIshida/hifuku)
+
+## pretrained model download
+```
+mkdir ~/.mohou
+cd ~/.mohou
+gdown "https://drive.google.com/uc?id=1EMi6aXgC1D0yvur4xCSRmfkLmXmvvs-I"
+tar -xvf hifuku-FetchTidyupTable-0.1.tar
+```
--- python/ext_examples/hifuku/fetch_demo.py
+++ python/ext_examples/hifuku/fetch_demo.py
@@ -1,41 +1,65 @@
+import numpy as np
 import time
-
-from plainmp.ompl_solver import OMPLSolver, OMPLSolverConfig
-from plainmp.robot_spec import FetchSpec
-from skmp.robot.utils import set_robot_state
-from skrobot.models.fetch import Fetch
-
+from rpbench.articulated.fetch.tidyup_table import TidyupTableTask
+from skrobot.models import Fetch
 from hifuku.domain import FetchTidyupTable
 from hifuku.script_utils import load_library
+from plainmp.robot_spec import FetchSpec
+from plainmp.ompl_solver import OMPLSolver, OMPLSolverConfig
+from plainmp.utils import set_robot_state
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
 
-domain = FetchTidyupTable
-task_type = domain.task_type
-task = task_type.sample()
-lib = load_library(domain, "cuda", postfix="0.1")
-# lib.jit_compile()
+# task creation
+table_pos = np.array([0.7, 0.0])
+bbox_param_list = [
+    np.array([0.8, 0.2, 0.4, 0.1, 0.1, 0.2]),
+    np.array([0.6, -0.4, 0.0, 0.1, 0.2, 0.25]),
+]
+target_xyz_yaw = np.array([0.6, 0.0, 0.85, 0.0])
+task = TidyupTableTask.from_semantic_params(table_pos, bbox_param_list, target_xyz_yaw)
 
-conf = OMPLSolverConfig(n_max_satisfaction_trial=1, n_max_call=10000)
-solver = OMPLSolver(conf)
-fs = FetchSpec()
+# you can solve it without trajectory library
+problem = task.export_problem()
+solver = OMPLSolver(OMPLSolverConfig())
+ret = solver.solve(problem)
+assert ret.traj is not None
+print(f"naive: solved in {1000 * ret.time_elapsed} [ms]")
+
+# load library (jit compilation makes inference 20x faster)
+jit_compile = False
+lib = load_library(FetchTidyupTable, "cuda", postfix="0.1")
+if jit_compile:
+    lib.jit_compile()
+    # warm up jit requires two calls (why two...?). This takes over 10 sec.
+    lib.infer(TidyupTableTask.sample())
+    lib.infer(TidyupTableTask.sample())
+    print("finished jit compile")
 
 ts = time.time()
 infer_res = lib.infer(task)
-print(f"infer time: {time.time() - ts}")
-print(f"Cost: {infer_res.cost}, Max admissible cost: {lib.max_admissible_cost}")
+print(f"elapsed time for inference: {time.time() - ts}")
+
 if infer_res.cost > lib.max_admissible_cost:
     print(f"sampled task seems to infesible")
 else:
     print(f"sampled task seems to feasiblem, so solve it")
+    conf = OMPLSolverConfig(n_max_ik_trial=1)
+    solver = OMPLSolver(conf)
     ret = solver.solve(task.export_problem(), infer_res.init_solution)
     assert ret.traj is not None
-    print(f"elapsed time: {ret.time_elapsed}")
+    print(f"learned: solved in {1000 * ret.time_elapsed} [ms]")
+
+    # visualize
+    fs = FetchSpec()
+    robot = Fetch()
+    robot.reset_pose()
     v = task.create_viewer()
-    fetch = Fetch()
-    v.add(fetch)
+    v.add(robot)
     v.show()
-    time.sleep(0.5)
+    time.sleep(2)
     for q in ret.traj:
-        set_robot_state(fetch, fs.control_joint_names, q)
+        set_robot_state(robot, fs.control_joint_names, q)
         v.redraw()
         time.sleep(0.3)
-    time.sleep(10000.0)
+    time.sleep(100)
