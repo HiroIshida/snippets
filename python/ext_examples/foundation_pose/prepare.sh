56d8790194e1244067de1c4bcbae22ad01793875
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-08-23 15:39:12 +0900

    Prepare model data of foundation pose

---
+++ python/ext_examples/foundation_pose/prepare.sh
@@ -0,0 +1,7 @@
+# model preparation
+mkdir demo_data
+gdown 1AwV9sESDKMgXGUu2n1o0Pc4x2JGYdVB3 -O demo_data/mustard0.zip
+mkdir weights
+gdown 1Zdjnkn4EHOI5_k08apofwRgTjWpai4E4 -O weights/model_best.pth
+gdown 1kQkQG-q_VvLRozv30hyeLB7P_jiEEqiE -O weights/config.yml
+
