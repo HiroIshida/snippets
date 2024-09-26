6535ab3efce6a27c9586eb23bc448eb9e0cfc3a1
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-09-26 10:57:19 +0900

    Addressing heap memory

--- asm/memory_addressing/main.cpp
+++ asm/memory_addressing/main.cpp
@@ -2,10 +2,21 @@
 #include <cstdint>
 
 extern "C" int32_t access_rdata(int32_t idx);
+extern "C" int64_t access_heap(int64_t* arr, int64_t idx);
 
 int main() {
+  // access rdata section
   for(size_t i = 0; i < 10; i++) {
     auto ret = access_rdata(i);
+    std::cout << "index: " << i << " value: " << ret << std::endl;
+  }
+
+  int64_t *heap = new int64_t[100];
+  for(size_t i = 0; i < 100; i++) {
+    heap[i] = i;
+  }
+  for(size_t i = 0; i < 10; i++) {
+    auto ret = access_heap(heap, i);
     std::cout << "index: " << i << " value: " << ret << std::endl;
   }
 }
--- asm/memory_addressing/memory.s
+++ asm/memory_addressing/memory.s
@@ -3,14 +3,22 @@ section .rdata
 PrimesNums: dd 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
 
 section .text
-global access_rdata
-;; int32_t access_rdata(int32_t index);
+;; just note to myself
 ;; rdi, rsi, rdx, rcx, r8, r9
 ;; edi, esi, edx, ecx, r8d, r9d
+
+global access_rdata
+;; int32_t access_rdata(int32_t index);
 access_rdata:
 movsxd r10, edi
 shl r10, 2
 lea r11, [PrimesNums]
 add r11, r10
 mov eax, [r11]
+ret
+
+global access_heap
+;; int64_t access_heap(int64_t* arr, int64_t index);
+access_heap:
+mov rax, [rdi + rsi * 8]
 ret
