f2c5fc9610ee48108884ac0172c7ebee7f616eec
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-10-27 17:38:47 +0900

    bench movsd vs movapd

--- asm/memory_benchmark/asmgen.py
+++ asm/memory_benchmark/asmgen.py
@@ -1,31 +1,43 @@
 n_iter = 100000
 
+def create_xmm_xmm_bench(instr_name):
+    # define movsd_xmm_xmm_bench
+    asm_string = ""
+    function_name = f"{instr_name}_xmm_xmm_bench"
+    asm_string += f"global {function_name}\n"
+    asm_string += f"{function_name}:\n"
+    for _ in range(n_iter):
+        asm_string += f"{instr_name} xmm0, xmm1\n"
+        asm_string += f"{instr_name} xmm1, xmm0\n"
+    asm_string += "ret\n"
+    asm_string += "\n"
+    return asm_string
+
+
+def create_xmm_stack_bench(instr_name):
+    # define movsd_xmm_mem_bench
+    asm_string = ""
+    function_name = f"{instr_name}_xmm_stack_bench"
+    asm_string += f"global {function_name}\n"
+    asm_string += f"{function_name}:\n"
+
+    asm_string += "push rbp\n"
+    asm_string += "mov rbp, rsp\n"
+    asm_string += "sub rsp, 16\n"  # 16 bytes for xmm alignment
+    for _ in range(n_iter):
+        asm_string += f"{instr_name} xmm0, [rsp]\n"
+        asm_string += f"{instr_name} [rsp], xmm0\n"
+    asm_string += "add rsp, 16\n"
+    asm_string += "pop rbp\n"
+    asm_string += "ret\n"
+    asm_string += "\n"
+    return asm_string
+
 asm_string = "section .text\n"
-
-# define movsd_xmm_xmm_bench
-function_name = "movsd_xmm_xmm_bench"
-asm_string += f"global {function_name}\n"
-asm_string += "movsd_xmm_xmm_bench:\n"
-for _ in range(n_iter):
-    asm_string += "movsd xmm0, xmm1\n"
-    asm_string += "movsd xmm1, xmm0\n"
-asm_string += "ret\n"
-asm_string += "\n"
-
-# define movsd_xmm_mem_bench
-function_name = "movsd_xmm_stack_bench"
-asm_string += f"global {function_name}\n"
-asm_string += "movsd_xmm_stack_bench:\n"
-
-asm_string += "push rbp\n"
-asm_string += "mov rbp, rsp\n"
-asm_string += "sub rsp, 8\n"
-for _ in range(n_iter):
-    asm_string += "movsd xmm0, [rsp]\n"
-    asm_string += "movsd [rsp], xmm0\n"
-asm_string += "add rsp, 8\n"
-asm_string += "pop rbp\n"
-asm_string += "ret\n"
+asm_string += create_xmm_xmm_bench("movsd")
+asm_string += create_xmm_stack_bench("movsd")
+asm_string += create_xmm_xmm_bench("movapd")
+asm_string += create_xmm_stack_bench("movapd")
 
 with open("bench.s", "w") as f:
     f.write(asm_string)
--- asm/memory_benchmark/main.cpp
+++ asm/memory_benchmark/main.cpp
@@ -2,26 +2,51 @@
 #include <iostream>
 extern "C" void movsd_xmm_xmm_bench();
 extern "C" void movsd_xmm_stack_bench();
+extern "C" void movapd_xmm_xmm_bench();
+extern "C" void movapd_xmm_stack_bench();
 
 int main() {
+  int n_here = 1000;
+  int n = 200000 * n_here;  // 200000 inside the loop (2 for round-trip)
   {
     std::cout << "movsd xmm <=> xmm\n";
     auto start = std::chrono::high_resolution_clock::now();
-    for(int i = 0; i < 1000; i++){
+    for(int i = 0; i < n_here; i++){
       movsd_xmm_xmm_bench();
     }
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
-    std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+    std::cout << "Elapsed time per instr: " << elapsed.count() / n * 1e9 << " ns\n";
   }
   {
     std::cout << "movsd xmm <=> stack\n";
     auto start = std::chrono::high_resolution_clock::now();
-    for(int i = 0; i < 1000; i++){
+    for(int i = 0; i < n_here; i++){
       movsd_xmm_stack_bench();
     }
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
-    std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+    std::cout << "Elapsed time per instr: " << elapsed.count() / n * 1e9 << " ns\n";
+  }
+
+  {
+    std::cout << "movapd xmm <=> xmm\n";
+    auto start = std::chrono::high_resolution_clock::now();
+    for(int i = 0; i < n_here; i++){
+      movapd_xmm_xmm_bench();
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "Elapsed time per instr: " << elapsed.count() / n * 1e9 << " ns\n";
+  }
+  {
+    std::cout << "movapd xmm <=> stack\n";
+    auto start = std::chrono::high_resolution_clock::now();
+    for(int i = 0; i < n_here; i++){
+      movapd_xmm_stack_bench();
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "Elapsed time per instr: " << elapsed.count() / n * 1e9 << " ns\n";
   }
 }
