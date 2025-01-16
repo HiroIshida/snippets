289635c0262951d7933e0d6350c3d05dfd5c7b43
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2025-01-16 22:34:10 +0900

    Add pyo3 class

--- rust/pyo3_example/README.md
+++ rust/pyo3_example/README.md
@@ -1,3 +1,6 @@
+```
 maturin build
-pip install target/wheels/my_pyo3_project-0.1.0-...-any.whl
+pip install target/wheels/my_pyo3_project-0.1.0-cp38-cp38-manylinux_2_28_x86_64.whl  --force-reinstall
 python -c "import my_pyo3_project; print(my_pyo3_project.hello_from_rust('PyO3'))"
+python -c "import my_pyo3_project; a = my_pyo3_project.MyRustClass('unko'); print(a.greet())"
+```
--- rust/pyo3_example/src/lib.rs
+++ rust/pyo3_example/src/lib.rs
@@ -5,8 +5,29 @@ fn hello_from_rust(name: &str) -> PyResult<String> {
     Ok(format!("Hello, {}! This is Rust talking.", name))
 }
 
+#[pyclass]
+struct MyRustClass {
+    name: String,
+}
+
+#[pymethods]
+impl MyRustClass {
+    #[new]
+    fn new(name: String) -> Self {
+        MyRustClass { name }
+    }
+
+    fn greet(&self) -> PyResult<String> {
+        Ok(format!(
+            "Hello, {}! MyRustClass is greeting you from Rust.",
+            self.name
+        ))
+    }
+}
+
 #[pymodule]
 fn my_pyo3_project(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(hello_from_rust, m)?)?;
+    m.add_class::<MyRustClass>()?;
     Ok(())
 }
