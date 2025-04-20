57a248d6dcfe56440e4986b9f70acd4984545044
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2025-04-20 14:06:08 +0900

    non blocking can send/recv

--- linux_tools/socketcan/emulation/linux_side.py
+++ linux_tools/socketcan/emulation/linux_side.py
@@ -24,6 +24,19 @@ def build_mit_frame():
     b7 = t_ff & 0xFF
     return [b0, b1, b2, b3, b4, b5, b6, b7]
 
+
+def decode_feedback(data):
+    d0 = data[0]
+    can_id =  d0 & 0x0F
+    err    = (d0 >> 4) & 0x0F
+    pos = ((data[1] << 8) | data[2]) & 0x3FFF
+    vel = ((data[3] << 4) | (data[4] >> 4)) & 0x0FFF
+    torque = (((data[4] & 0x0F) << 8) | data[5]) & 0x0FFF
+    t_mos = data[6]
+    t_rot = data[7]
+    return can_id, err, pos, vel, torque, t_mos, t_rot
+
+
 def main_brocking():
     duration = 0.001
     while True:
@@ -39,12 +52,48 @@ def main_brocking():
                 print("fail")
             msg = bus.recv(timeout=0.1)
         elapsed = time.time() - ts
-        print(f"elapsed: {elapsed}s")
+        hz = 1 / elapsed
+        print(f"hz: {hz:.2f} Hz")
         remain = duration - elapsed
         if remain < 0:
             print("Warning: send time exceeded duration")
         else:
             time.sleep(remain)
 
+def main_nonblocking():
+    duration = 0.001
+    while True:
+        ts = time.time()
+        data = build_mit_frame()
+        # batch send
+        for cid in control_ids:
+            msg = can.Message(arbitration_id=cid,
+                              data=bytearray(data),
+                              is_extended_id=False)
+            try:
+                bus.send(msg)
+            except can.CanError:
+                print("fail")
+
+        # batch receive
+        for cid in control_ids:
+            msg = bus.recv(timeout=0.1)
+            if msg is not None:
+                can_id, err, pos, vel, torque, t_mos, t_rot = decode_feedback(msg.data)
+                print(f"ID: {can_id}, err: {err}, pos: {pos}, vel: {vel}, torque: {torque}, t_mos: {t_mos}, t_rot: {t_rot}")
+            else:
+                print("No message received")
+
+        elapsed = time.time() - ts
+        hz = 1 / elapsed
+        print(f"hz: {hz:.2f} Hz")
+        remain = duration - elapsed
+        if remain < 0:
+            print("Warning: send time exceeded duration")
+        else:
+            time.sleep(remain)
+
+
 if __name__ == "__main__":
-    main_brocking()
+    # main_brocking()
+    main_nonblocking()
--- linux_tools/socketcan/emulation/motor_side.py
+++ linux_tools/socketcan/emulation/motor_side.py
@@ -1,30 +1,32 @@
 import time
+import numpy as np
 import can
 import struct
 import random
 import argparse
 
 
-def build_feedback(err=1):
-    pos = random.randint(0, 0x3FFF)      # 14bit
-    vel = random.randint(0, 0x0FFF)      # 12bit
-    torque = random.randint(0, 0x0FFF)   # 12bit
-    t_mos = random.randint(20,40)
-    t_rot = random.randint(20,40)
-
-    d0 = (MASTER_ID & 0xFF) | ((err & 0x0F) << 4)
-    d1 = (pos >> 8) & 0xFF
-    d2 = pos & 0xFF
-    d3 = (vel >> 4) & 0xFF
-    d4 = ((vel & 0x0F) << 4) | ((torque >> 8) & 0x0F)
-    d5 = torque & 0xFF
-    return [d0, d1, d2, d3, d4, d5, t_mos, t_rot]
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--id", type=int, default=0x01, help="CAN ID of the motor")
     args = parser.parse_args()
     can_id = args.id
+
+    def build_feedback(err=1):
+        pos = random.randint(0, 0x3FFF)      # 14bit
+        vel = random.randint(0, 0x0FFF)      # 12bit
+        torque = random.randint(0, 0x0FFF)   # 12bit
+        t_mos = random.randint(20,40)
+        t_rot = random.randint(20,40)
+
+        d0 = (can_id & 0xFF) | ((err & 0x0F) << 4)
+        d1 = (pos >> 8) & 0xFF
+        d2 = pos & 0xFF
+        d3 = (vel >> 4) & 0xFF
+        d4 = ((vel & 0x0F) << 4) | ((torque >> 8) & 0x0F)
+        d5 = torque & 0xFF
+        return [d0, d1, d2, d3, d4, d5, t_mos, t_rot]
+
 
     bus = can.interface.Bus('vcan0', bustype='socketcan')
     bus.set_filters([{"can_id": can_id, "can_mask": 0x7FF}])  # all bits are relevant
@@ -37,5 +39,6 @@ if __name__ == "__main__":
         feedback = build_feedback()
         rsp = can.Message(arbitration_id=MASTER_ID,
                           data=feedback, is_extended_id=False)
-        time.sleep(0.0002)  # simulate delay in motor response
+        sleep_duration = np.random.uniform(0.0001, 0.0005)
+        time.sleep(sleep_duration)  # simulate delay in motor response
         bus.send(rsp)
